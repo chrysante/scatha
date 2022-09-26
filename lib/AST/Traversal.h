23b94703ad4652ff82866d45bb7f4af5b8a7d883
chrysante <gruenlilie@protonmail.com>
2022-09-27 01:13:53 +0200

    boolean literal case added

--- lib/AST/Traversal.h
+++ lib/AST/Traversal.h
@@ -161,6 +161,12 @@ namespace scatha::ast::internal {
 					post(node);
 					break;
 				}
+				case NodeType::BooleanLiteral: {
+					auto const* const node = static_cast<BooleanLiteral const*>(inNode);
+					pre(node);
+					post(node);
+					break;
+				}
 				case NodeType::FloatingPointLiteral: {
 					auto const* const node = static_cast<FloatingPointLiteral const*>(inNode);
 					pre(node);
