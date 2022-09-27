6052d24c5b5487877b03fdc7a7fe4f788289577e
chrysante <gruenlilie@protonmail.com>
2022-09-27 11:17:58 +0200

    separated test cases

--- test/CodeGen/CodeGenerator.t.cc
+++ test/CodeGen/CodeGenerator.t.cc
@@ -26,9 +26,9 @@ fn main() -> int {
 
 TEST_CASE("Simplest non-trivial program", "[codegen]") {
 	std::string const text = R"(
-	fn main() -> int {
-		return 1;
-	}
+fn main() -> int {
+	return 1;
+}
 	)";
 	
 	auto const vm = test::compileAndExecute(text);
@@ -36,127 +36,138 @@ TEST_CASE("Simplest non-trivial program", "[codegen]") {
 	CHECK(state.registers[0] == 1);
 }
 
-TEST_CASE("Simple arithmetic", "[codegen]") {
-	std::string text;
-	u64 expectation = 0;
-	SECTION("Addition") {
-		text = R"(
-	fn main() -> int {
-		let a = 1;
-		let b = 2;
-		return a + b;
-	}
-	)";
-		expectation = 3;
-	}
-	SECTION("Subtraction") {
-		text = R"(
-	fn main() -> int {
-		let a = 1;
-		let b = 2;
-		return a - b;
-	}
-	)";
-		expectation = static_cast<u64>(-1);
-	}
-	SECTION("Multiplication") {
-		text = R"(
-	fn main() -> int {
-		let a = 4;
-		let b = -23;
-		return a * b;
-	}
-	)";
-		expectation = static_cast<u64>(-92);
-	}
-	SECTION("Division") {
-		text = R"(
-	fn main() -> int {
-		let a = 100;
-		let b = 4;
-		return a / b;
-	}
-	)";
-		expectation = 25;
-	}
-	SECTION("Remainder") {
-		text = R"(
-	fn main() -> int {
-		let a = 100;
-		let b = 17;
-		return a % b;
-	}
-	)";
-		expectation = 15;
-	}
-	SECTION("Float Addition") {
-		text = R"(
-	fn test() -> float {
-		let a = 1.3;
-		let b = 2.3;
-		return a + b;
-	}
-	)";
-		expectation = utl::bit_cast<u64>(1.3 + 2.3);
-	}
-	SECTION("Float Mutliplication") {
-		text = R"(
-	fn test() -> float {
-		let a = 1.3;
-		let b = 2.3;
-		return a * b;
-	}
-	)";
-		expectation = utl::bit_cast<u64>(1.3 * 2.3);
-	}
-	SECTION("Float Subtraction") {
-		text = R"(
-	fn test() -> float {
-		let a = 1.4;
-		let b = 2.3;
-		return a - b;
-	}
-	)";
-		expectation = utl::bit_cast<u64>(1.4 - 2.3);
-	}
-	SECTION("Float Division") {
-		text = R"(
-	fn test() -> float {
-		let a = 1.4;
-		let b = 2.3;
-		return a / b;
-	}
-	)";
-		expectation = utl::bit_cast<u64>(1.4 / 2.3);
-	}
-	
-	SECTION("More complex expressions") {
-		text = R"(
-	fn main() -> int {
-		let a = 12;
-		let b = 2;
-		let c = 4;
-		return (a + b * c) / 2;
-	}
-	)";
-		expectation = 10;
-	}
-	
-	SECTION("Even more complex expressions") {
-		text = R"(
-	fn main() -> int {
-		let a = 12;
-		var b = 0;
-		let c = 4;
-		b += 2;
-		return (a + b * c) / 2;
-	}
-	)";
-		expectation = 10;
-	}
-	
-	auto const vm = test::compileAndExecute(text);
-	auto const& state = vm.getState();
-	CHECK(state.registers[0] == expectation);
+TEST_CASE("Addition", "[codegen]") {
+	std::string const text = R"(
+fn main() -> int {
+	let a = 1;
+	let b = 2;
+	return a + b;
+}
+)";
+	auto const registers = test::getRegisters(text);
+	CHECK(registers[0] == 3);
+}
+
+TEST_CASE("Subtraction", "[codegen]") {
+	std::string const text = R"(
+fn main() -> int {
+	let a = 1;
+	let b = 2;
+	return a - b;
+}
+)";
+	auto const registers = test::getRegisters(text);
+	CHECK(registers[0] == static_cast<u64>(-1));
+}
+
+TEST_CASE("Multiplication", "[codegen]") {
+	std::string const text = R"(
+fn main() -> int {
+	let a = 4;
+	let b = -23;
+	return a * b;
+}
+)";
+	auto const registers = test::getRegisters(text);
+	CHECK(registers[0] == static_cast<u64>(-92));
+}
+
+TEST_CASE("Division", "[codegen]") {
+	std::string const text = R"(
+fn main() -> int {
+	let a = 100;
+	let b = 4;
+	return a / b;
+}
+)";
+	auto const registers = test::getRegisters(text);
+	CHECK(registers[0] == 25);
+}
+
+TEST_CASE("Remainder", "[codegen]") {
+	std::string const text = R"(
+fn main() -> int {
+	let a = 100;
+	let b = 17;
+	return a % b;
+}
+)";
+	auto const registers = test::getRegisters(text);
+	CHECK(registers[0] == 15);
+}
+
+TEST_CASE("Float Addition", "[codegen]") {
+	std::string const text = R"(
+fn test() -> float {
+	let a = 1.3;
+	let b = 2.3;
+	return a + b;
+}
+)";
+	auto const registers = test::getRegisters(text);
+	CHECK(registers[0] == utl::bit_cast<u64>(1.3 + 2.3));
+}
+
+TEST_CASE("Float Mutliplication", "[codegen]") {
+	std::string const text = R"(
+fn test() -> float {
+	let a = 1.3;
+	let b = 2.3;
+	return a * b;
+}
+)";
+	auto const registers = test::getRegisters(text);
+	CHECK(registers[0] == utl::bit_cast<u64>(1.3 * 2.3));
+}
+
+TEST_CASE("Float Subtraction", "[codegen]") {
+	std::string const text = R"(
+fn test() -> float {
+	let a = 1.4;
+	let b = 2.3;
+	return a - b;
+}
+)";
+	auto const registers = test::getRegisters(text);
+	CHECK(registers[0] == utl::bit_cast<u64>(1.4 - 2.3));
+}
+
+TEST_CASE("Float Division", "[codegen]") {
+	std::string const text = R"(
+fn test() -> float {
+	let a = 1.4;
+	let b = 2.3;
+	return a / b;
+}
+)";
+	auto const registers = test::getRegisters(text);
+	CHECK(registers[0] == utl::bit_cast<u64>(1.4 / 2.3));
+}
+
+TEST_CASE("More complex expressions", "[codegen]") {
+	std::string const text = R"(
+fn main() -> int {
+	let a = 12;
+	let b = 2;
+	let c = 4;
+	return (a + b * c) / 2;
+}
+)";
+	auto const registers = test::getRegisters(text);
+	CHECK(registers[0] == 10);
+}
+
+TEST_CASE("Even more complex expressions", "[codegen]") {
+	std::string const text = R"(
+fn main() -> int {
+	let a = 12;
+	var b = 0;
+	let c = 4;
+	b += 2;
+	return (a + b * c) / 2;
+}
+)";
+	auto const registers = test::getRegisters(text);
+	CHECK(registers[0] == 10);
 }
 
