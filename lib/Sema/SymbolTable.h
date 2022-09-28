54dcb0595a199f2da71bc351cb531ed77d8bdce4
chrysante <gruenlilie@protonmail.com>
2022-09-28 14:20:57 +0200

    fixed documentation

--- lib/Sema/SymbolTable.h
+++ lib/Sema/SymbolTable.h
@@ -16,7 +16,7 @@ namespace scatha::sema {
 	 Tree of all scopes and names in a translation unit
 	 
 	 # Notes: #
-	 1. call addSymbol() to add a new name to the current scope. Must be called before using pushScope with that name
+	 1. call \p addSymbol() to add a new name to the current scope. Must be called before using \p pushScope with that name
 	 */
 	class SCATHA(API) SymbolTable {
 	public:
@@ -25,10 +25,10 @@ namespace scatha::sema {
 		/**
 		 Add a symbol to the current scope.
 		 
-		 - parameter \p name: Name of the symbol to add to the current scope.
-		 - parameter \p category: Category of the symbol.
+		 - parameter name Name of the symbol to add to the current scope.
+		 - parameter category Category of the symbol.
 		 
-		 - returns: Pair of ID of the added or existing name and boolean wether the name already existed.
+		 - returns Pair of ID of the added or existing name and boolean wether the name already existed.
 		 
 		 # Notes: #
 		 1. \p name may already exist in the current scope. Then it is verified that \p category is the same as for the last call to this function.
@@ -39,9 +39,9 @@ namespace scatha::sema {
 		/**
 		 Add an anonymous symbol to the current scope.
 		 
-		 - parameter \p category: Category of the symbol.
+		 - parameter category Category of the symbol.
 		 
-		 - returns: Pair of ID of the added or existing name and boolean wether the name already existed.
+		 - returns Pair of ID of the added or existing name and boolean wether the name already existed.
 		 
 		 # Notes: #
 		 1. \p name may already exist in the current scope. Then it is verified that \p category is the same as for the last call to this function.
@@ -52,9 +52,9 @@ namespace scatha::sema {
 		/**
 		 Make current one of the child scopes of the current scope.
 		 
-		 - parameter \p name: Name of the scope to make current.
+		 - parameter name Name of the scope to make current.
 		 
-		 - warning: \p name must be a child scope of the current scope.
+		 - warning \p name must be a child scope of the current scope.
 		 */
 		void pushScope(std::string_view name);
 		void pushScope(SymbolID name);
@@ -62,16 +62,16 @@ namespace scatha::sema {
 		/**
 		 Make current the parent scope of the current scope.
 		 
-		 - warning: Current scope must not be the global scope.
+		 - warning Current scope must not be the global scope.
 		 */
 		void popScope();
 		
 		/**
 		 Declare a type in the current scope.
 		 
-		 - parameter \p name: Name of the type.
+		 - parameter name Name of the type.
 		 
-		 - returns: ID of the added or existing name.
+		 - returns ID of the added or existing name.
 		 
 		 # Notes: #
 		 1. \p name may already exist, however must be of category type.
@@ -82,11 +82,11 @@ namespace scatha::sema {
 		/**
 		 Define a type in the current scope.
 		 
-		 - parameter \p name: Name of the type.
-		 - parameter \p size: Size of the type in bytes.
-		 - parameter \p align: Alignment of the type in bytes.
+		 - parameter name Name of the type.
+		 - parameter size Size of the type in bytes.
+		 - parameter align Alignment of the type in bytes.
 		 
-		 - returns: Reference to the added type object.
+		 - returns Reference to the added type object.
 		 
 		 # Notes: #
 		 1. \p name may already exist, however must not yet be defined and must be of category type.
@@ -97,13 +97,26 @@ namespace scatha::sema {
 		TypeEx& defineBuiltinType(Token const& name, size_t size, size_t align);
 		
 		/**
+		 Declare an overload set in the current scope.
+		 
+		 - parameter name Name of the overload set.
+		 
+		 - returns Pair of pointer to the added or existing overload set and boolean wether the name already existed.
+		 
+		 # Notes: #
+		 1. \p name may already exist, however must be of category OverloadSet.
+		 So this function may be called multiple times with the same name, however the signature must match.
+		 */
+		std::pair<Function*, bool> declareOverloadSet(Token const& name, TypeID returnType, std::span<TypeID const> argumentTypes);
+		
+		/**
 		 Declare a function in the current scope.
 		 
-		 - parameter \p name: Name of the function.
-		 - parameter \p returnType: TypeID of the returned type.
-		 - parameter \p argumentTypes: TypeIDs of the arguments.
+		 - parameter name Name of the function.
+		 - parameter returnType TypeID of the returned type.
+		 - parameter argumentTypes TypeIDs of the arguments.
 		 
-		 - returns: Pair of pointer to the added or existing function and boolean wether the name already existed.
+		 - returns Pair of pointer to the added or existing function and boolean wether the name already existed.
 		 
 		 # Notes: #
 		 1. \p name may already exist, however must be of category function.
@@ -114,11 +127,11 @@ namespace scatha::sema {
 		/**
 		 Declare a variable in the current scope.
 		 
-		 - parameter \p name: Name of the variable.
-		 - parameter \p typeID: TypeID of the variable.
-		 - parameter \p isConstant: boolean indicating constness.
+		 - parameter name Name of the variable.
+		 - parameter typeID TypeID of the variable.
+		 - parameter isConstant boolean indicating constness.
 		 
-		 - returns: Pair of pointer to the added or existing variable and boolean wether the name already existed.
+		 - returns Pair of pointer to the added or existing variable and boolean wether the name already existed.
 		 
 		 # Notes: #
 		 1. \p name may already exist, however must be of category variable.
@@ -129,9 +142,9 @@ namespace scatha::sema {
 		/**
 		 Performs scoped name lookup. Looks for the name in the current scope and walks up the scope tree until it finds the name or reaches the global scope.
 		 
-		 - parameter \p name: Name to look for.
+		 - parameter name Name to look for.
 		 
-		 - returns: ID of the found symbol or invalidSymbolID if not found.
+		 - returns ID of the found symbol or invalidSymbolID if not found.
 		 */
 		SymbolID lookupName(Token const& name) const;
 		
