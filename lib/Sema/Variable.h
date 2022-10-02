5429c735805de0cd6673644a8c8efbc6524f2a4a
chrysante <gruenlilie@protonmail.com>
2022-10-02 02:09:17 +0200

    constness as property of the variable. need to remove this again

--- lib/Sema/Variable.h
+++ lib/Sema/Variable.h
@@ -7,15 +7,19 @@ namespace scatha::sema {
 	
 	class Variable: public EntityBase {
 	public:
-		explicit Variable(std::string name, SymbolID symbolID, TypeID typeID):
+		explicit Variable(std::string name, SymbolID symbolID, TypeID typeID, bool isConstant):
 			EntityBase(std::move(name), symbolID),
-			_typeID(typeID)
+			_typeID(typeID),
+			_isConstant(isConstant)
 		{}
 		
 		TypeID typeID() const { return _typeID; }
 		
+		bool isConstant() const { return _isConstant; }
+		
 	private:
 		TypeID _typeID;
+		bool _isConstant;
 	};
 	
 }
