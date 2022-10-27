ad493f37a7c931d2f1f523a54cf53b02478bb5b5
chrysante <gruenlilie@protonmail.com>
2022-10-27 21:03:16 +0200

    Refactored canonicalizer

--- lib/IC/Canonicalize.cc
+++ lib/IC/Canonicalize.cc
@@ -9,98 +9,102 @@
 namespace scatha::ic {
 
 namespace {
+
 struct Context {
-    void run(ast::AbstractSyntaxTree&, ast::NodeType);
-    void run(ast::AbstractSyntaxTree& node) { run(node, node.nodeType()); }
+    void dispatch(ast::AbstractSyntaxTree&);
+
+    void canonicalize(auto&);
+    void canonicalize(ast::IfStatement&);
+    void canonicalize(ast::BinaryExpression&);
+
     void doCompoundAssignment(ast::BinaryExpression& node, ast::BinaryOperator to) const;
 };
+
 } // namespace
 
-void Context::run(ast::AbstractSyntaxTree& node, ast::NodeType type) {
-    ast::visit(node,
-               type,
-               utl::visitor{ [&](ast::IfStatement& is) {
-                                run(*is.condition);
-                                run(*is.ifBlock);
-                                if (is.elseBlock != nullptr) {
-                                    run(*is.elseBlock);
-                                }
+void Context::dispatch(ast::AbstractSyntaxTree& node) {
+    return ast::visit(node, [this](auto& node) { canonicalize(node); });
+}
 
-                                /*
-                                 * If we have a statement of type if (!<boolean-expr>) {} else
-                                 * {} convert the expression to <boolean-expr> and swap the if
-                                 * and else block. These kind of if statements will appear quite
-                                 * often after canonicalizing the condition expressions.
-                                 */
-                                if (is.condition->nodeType() != ast::NodeType::UnaryPrefixExpression) {
-                                    return;
-                                }
-                                auto& un = *static_cast<ast::UnaryPrefixExpression*>(is.condition.get());
-                                if (un.op != ast::UnaryPrefixOperator::LogicalNot) {
-                                    return;
-                                }
-                                is.condition = std::move(un.operand);
-                                std::swap(is.ifBlock, is.elseBlock);
-                            },
-                             [&](ast::BinaryExpression& expr) {
-        run(*expr.lhs);
-        run(*expr.rhs);
+void Context::canonicalize(auto& node) {
+    ast::DefaultCase([this](ast::AbstractSyntaxTree& node) { dispatch(node); })(node);
+}
 
-        switch (expr.op) {
-        case ast::BinaryOperator::Greater: {
-            expr.op = ast::BinaryOperator::Less;
-            std::swap(expr.lhs, expr.rhs);
-            break;
-        }
-        case ast::BinaryOperator::GreaterEq: {
-            expr.op = ast::BinaryOperator::LessEq;
-            std::swap(expr.lhs, expr.rhs);
-            break;
-        }
-        case ast::BinaryOperator::AddAssignment: {
-            doCompoundAssignment(expr, ast::BinaryOperator::Addition);
-            break;
-        }
-        case ast::BinaryOperator::SubAssignment: {
-            doCompoundAssignment(expr, ast::BinaryOperator::Subtraction);
-            break;
-        }
-        case ast::BinaryOperator::MulAssignment: {
-            doCompoundAssignment(expr, ast::BinaryOperator::Multiplication);
-            break;
-        }
-        case ast::BinaryOperator::DivAssignment: {
-            doCompoundAssignment(expr, ast::BinaryOperator::Division);
-            break;
-        }
-        case ast::BinaryOperator::RemAssignment: {
-            doCompoundAssignment(expr, ast::BinaryOperator::Remainder);
-            break;
-        }
-        case ast::BinaryOperator::LSAssignment: {
-            doCompoundAssignment(expr, ast::BinaryOperator::LeftShift);
-            break;
-        }
-        case ast::BinaryOperator::RSAssignment: {
-            doCompoundAssignment(expr, ast::BinaryOperator::RightShift);
-            break;
-        }
-        case ast::BinaryOperator::AndAssignment: {
-            doCompoundAssignment(expr, ast::BinaryOperator::BitwiseAnd);
-            break;
-        }
-        case ast::BinaryOperator::OrAssignment: {
-            doCompoundAssignment(expr, ast::BinaryOperator::BitwiseOr);
-            break;
-        }
+void Context::canonicalize(ast::IfStatement& statement) {
+    dispatch(*statement.condition);
+    dispatch(*statement.ifBlock);
+    if (statement.elseBlock != nullptr) {
+        dispatch(*statement.elseBlock);
+    }
+    /// If we have a statement of type \code if (!<boolean-expr>) {} else {} \endcode
+    /// convert the expression to \p <boolean-expr> and swap the if
+    /// and else block. 
+    ///
+    if (statement.condition->nodeType() != ast::NodeType::UnaryPrefixExpression) {
+        return;
+    }
+    auto& un = *static_cast<ast::UnaryPrefixExpression*>(statement.condition.get());
+    if (un.op != ast::UnaryPrefixOperator::LogicalNot) {
+        return;
+    }
+    statement.condition = std::move(un.operand);
+    std::swap(statement.ifBlock, statement.elseBlock);
+}
 
-        default: break;
-        }
-                            },
-                             ast::DefaultCase(utl::visitor{
-                                 [&](ast::AbstractSyntaxTree& node) { run(node); },
-                                 [&](ast::AbstractSyntaxTree& node, ast::NodeType type) { run(node, type); },
-                             }) });
+void Context::canonicalize(ast::BinaryExpression& expr) {
+    dispatch(*expr.lhs);
+    dispatch(*expr.rhs);
+
+    switch (expr.op) {
+    case ast::BinaryOperator::Greater: {
+        expr.op = ast::BinaryOperator::Less;
+        std::swap(expr.lhs, expr.rhs);
+        break;
+    }
+    case ast::BinaryOperator::GreaterEq: {
+        expr.op = ast::BinaryOperator::LessEq;
+        std::swap(expr.lhs, expr.rhs);
+        break;
+    }
+    case ast::BinaryOperator::AddAssignment: {
+        doCompoundAssignment(expr, ast::BinaryOperator::Addition);
+        break;
+    }
+    case ast::BinaryOperator::SubAssignment: {
+        doCompoundAssignment(expr, ast::BinaryOperator::Subtraction);
+        break;
+    }
+    case ast::BinaryOperator::MulAssignment: {
+        doCompoundAssignment(expr, ast::BinaryOperator::Multiplication);
+        break;
+    }
+    case ast::BinaryOperator::DivAssignment: {
+        doCompoundAssignment(expr, ast::BinaryOperator::Division);
+        break;
+    }
+    case ast::BinaryOperator::RemAssignment: {
+        doCompoundAssignment(expr, ast::BinaryOperator::Remainder);
+        break;
+    }
+    case ast::BinaryOperator::LSAssignment: {
+        doCompoundAssignment(expr, ast::BinaryOperator::LeftShift);
+        break;
+    }
+    case ast::BinaryOperator::RSAssignment: {
+        doCompoundAssignment(expr, ast::BinaryOperator::RightShift);
+        break;
+    }
+    case ast::BinaryOperator::AndAssignment: {
+        doCompoundAssignment(expr, ast::BinaryOperator::BitwiseAnd);
+        break;
+    }
+    case ast::BinaryOperator::OrAssignment: {
+        doCompoundAssignment(expr, ast::BinaryOperator::BitwiseOr);
+        break;
+    }
+
+    default: break;
+    }
 }
 
 void Context::doCompoundAssignment(ast::BinaryExpression& node, ast::BinaryOperator to) const {
@@ -115,7 +119,7 @@ void Context::doCompoundAssignment(ast::BinaryExpression& node, ast::BinaryOpera
 
 void canonicalize(ast::AbstractSyntaxTree* node) {
     Context ctx;
-    ctx.run(*node);
+    ctx.dispatch(*node);
 }
 
 } // namespace scatha::ic
