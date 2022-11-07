59e5cbc22d180bf59f724c9e03ffdbde3316f463
chrysante <gruenlilie@protonmail.com>
2022-11-07 14:24:56 +0100

    Cleaned TokenStream class

--- lib/Parser/TokenStream.cc
+++ lib/Parser/TokenStream.cc
@@ -5,37 +5,9 @@ namespace scatha::parse {
 TokenStream::TokenStream(utl::vector<Token> tokens): tokens(std::move(tokens)) {}
 
 Token const& TokenStream::eat() {
-    return eatImpl(&index);
-}
-
-void TokenStream::advancePastSeparator() { SC_DEBUGFAIL(); }
-
-void TokenStream::advanceUntilStable() {
-    SC_ASSERT(index < (ssize_t)tokens.size(), "");
-    if (index == (ssize_t)tokens.size() - 1) { return; }
-    for (ssize_t numOpenBrace = 0, numOpenParan = 0, numOpenBracket = 0; index < (ssize_t)tokens.size(); ) {
-        Token const& next = peek();
-        if (next.id == "{") { ++numOpenBrace; }
-        if (next.id == "}") { --numOpenBrace; }
-        if (next.id == "(") { ++numOpenParan; }
-        if (next.id == ")") { --numOpenParan; }
-        if (next.id == "[") { ++numOpenBracket; }
-        if (next.id == "]") { --numOpenBracket; }
-        if (numOpenBrace <= 0 && numOpenParan == 0 && numOpenBracket == 0) {
-            /// Here we can potentially find a stable point to continue parsing
-            if (next.isSeparator) {
-//                if (next.type != TokenType::EndOfFile) { eat(); }
-                return;
-            }
-            if (next.isDeclarator) {
-                return;
-            }
-            if (next.id == "}" && numOpenBrace == -1) {
-                return;
-            }
-        }
-        eat();
-    }
+    Token const& result = eatImpl(&_index);
+    count(result);
+    return result;
 }
 
 bool TokenStream::advanceTo(std::string_view id) {
@@ -47,25 +19,21 @@ bool TokenStream::advanceTo(std::string_view id) {
     }
 }
 
-void TokenStream::insert(Token const& token) {
-    tokens.insert(tokens.begin() + index, token);
-}
-
 Token const& TokenStream::peek() {
-    ssize_t i = index;
+    ssize_t i = _index;
     return eatImpl(&i);
 }
 
 Token const& TokenStream::current() {
-    SC_ASSERT(index >= 0, "");
-    SC_ASSERT(index < (ssize_t)tokens.size(), "");
-    return tokens[(size_t)index];
+    SC_ASSERT(_index >= 0, "");
+    SC_ASSERT(_index < (ssize_t)tokens.size(), "");
+    return tokens[(size_t)_index];
 }
 
 Token const& TokenStream::eatImpl(ssize_t* i) {
     SC_ASSERT(*i >= -1, "");
     if (*i >= (ssize_t)tokens.size() - 1) {
-        index = (ssize_t)tokens.size();
+        _index = (ssize_t)tokens.size();
         return tokens.back();
     }
 //    SC_ASSERT(*i < (ssize_t)tokens.size() - 1, "");
--- lib/Parser/TokenStream.h
+++ lib/Parser/TokenStream.h
@@ -5,6 +5,7 @@
 
 #include "Basic/Basic.h"
 #include "Common/Token.h"
+#include "Parser/ParanCounter.h"
 
 namespace scatha::parse {
 
@@ -14,17 +15,15 @@ namespace scatha::parse {
 ///
 /// \details
 /// 1. Expects the last token in the stream to be of type EndOfFile.
-class SCATHA(API) TokenStream {
+class SCATHA(API) TokenStream: private ParanCounter {
 public:
     /// Constructs an empty TokenStream.
     TokenStream() = default;
-    
     
     /// Constructs a TokenStream from the given Range of Token's.
     ///
     /// \param tokens Vector of tokens.
     explicit TokenStream(utl::vector<Token> tokens);
-
     
     /// Extract one token from the stream.
     ///
@@ -36,23 +35,11 @@ public:
     /// 2. When called in a loop, the stream will be iterated.
     Token const& eat();
     
-    
-    /// Advance the token stream past the next separator.
-    /// After calling this function a call to \p current() will return a separator token or the next closing brace '}'.
-    [[deprecated]] void advancePastSeparator();
-    
-    /// Call on syntax errors.  Advances the token stream to a position where we can continue parsing.
-    /// Advances past matching pairs of braces, parantheses and brackets.
-    /// After calling this function a call to \p peek() will return  a separator token or a declarator or a closing brace '}'.
-    void advanceUntilStable();
-    
     /// Advance the token stream to the next token with \code token.id == id \endcode or past the next separator.
     /// After calling this function a call to \p current() will return the next token with specified id or a  separator token.
     ///
     /// \returns \p true iff \p id was found before a separator.
     bool advanceTo(std::string_view id);
-    
-    void insert(Token const& token);
     
     /// Look ahead one token into the stream.
     ///
@@ -65,7 +52,6 @@ public:
     /// 2. Always returns the same token when called in a loop.
     Token const& peek();
 
-    
     /// \returns A reference to the current token in the stream aka the token
     /// returned by the last call to \p eat().
     ///
@@ -74,12 +60,20 @@ public:
     /// 2. Always returns the same token when called in a loop.
     Token const& current();
 
+    ssize_t index() const { return _index; }
+    
+    ssize_t size() const { return (ssize_t)tokens.size(); }
+    
+    using ParanCounter::parans;
+    using ParanCounter::brackets;
+    using ParanCounter::braces;
+    
 private:
     Token const& eatImpl(ssize_t*);
 
 private:
     utl::vector<Token> tokens;
-    ssize_t index = -1;
+    ssize_t _index = -1;
 };
 
 } // namespace scatha::parse
