079012adde32bfc64858ad5c1b4bb7b697136219
chrysante <gruenlilie@protonmail.com>
2022-12-02 15:53:49 +0100

    Fixed C-style casts

--- test/Common/Allocator.t.cc
+++ test/Common/Allocator.t.cc
@@ -7,17 +7,17 @@ using namespace scatha;
 TEST_CASE("alignPointer") {
     using namespace internal;
     u8* p;
-    p = (u8*)16;
+    p = reinterpret_cast<u8*>(16);
     CHECK(alignPointer(p, 1) == p);
     CHECK(alignPointer(p, 4) == p);
     CHECK(alignPointer(p, 8) == p);
     CHECK(alignPointer(p, 16) == p);
-    p = (u8*)4;
+    p = reinterpret_cast<u8*>(4);
     CHECK(alignPointer(p, 1) == p);
     CHECK(alignPointer(p, 4) == p);
     CHECK(alignPointer(p, 8) == p + 4);
     CHECK(alignPointer(p, 16) == p + 12);
-    p = (u8*)3;
+    p = reinterpret_cast<u8*>(3);
     CHECK(alignPointer(p, 1) == p);
     CHECK(alignPointer(p, 4) == p + 1);
     CHECK(alignPointer(p, 8) == p + 5);
@@ -27,8 +27,8 @@ TEST_CASE("alignPointer") {
 TEST_CASE("MonotonicBufferAllocator") {
     MonotonicBufferAllocator alloc(128);
     for (int i = 0; i < 100; ++i) {
-        u8* ptr = (u8*)alloc.allocate(16, 8);
-        CHECK((size_t)ptr % 8 == 0);
+        u8* ptr = static_cast<u8*>(alloc.allocate(16, 8));
+        CHECK(reinterpret_cast<size_t>(ptr) % 8 == 0);
         std::memset(ptr, 0, 16);
     }
 }
