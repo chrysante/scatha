750323cee5493968fceb3ff0442a30e6fb245787
chrysante <gruenlilie@protonmail.com>
2022-12-02 15:51:41 +0100

    Better operator== and strong ordering for SourceLocation

--- lib/Common/SourceLocation.h
+++ lib/Common/SourceLocation.h
@@ -3,6 +3,7 @@
 #ifndef SCATHA_COMMON_SOURCELOCATION_H_
 #define SCATHA_COMMON_SOURCELOCATION_H_
 
+#include <compare>
 #include <iosfwd>
 
 #include "Basic/Basic.h"
@@ -12,9 +13,20 @@ namespace scatha {
 struct SourceLocation {
     i64 index = 0;
     i32 line = 0, column = 0;
-    
-    bool operator==(SourceLocation const&) const = default;
 };
+
+inline bool operator==(SourceLocation const& lhs, SourceLocation const& rhs) noexcept {
+    bool const result = lhs.index == rhs.index;
+    if (result) {
+        SC_ASSERT(lhs.line == rhs.line, "Line number must match for the same index.");
+        SC_ASSERT(lhs.column == rhs.column, "Column number must match for the same index.");
+    }
+    return result;
+}
+
+inline std::strong_ordering operator<=>(SourceLocation const& lhs, SourceLocation const& rhs) noexcept {
+    return lhs.index <=> rhs.index;
+}
 
 SCATHA(API) std::ostream& operator<<(std::ostream&, SourceLocation const&);
 
