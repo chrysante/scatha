b7d216af8c6c5fb62d74924b90d69cde09ad7ad7
chrysante <gruenlilie@protonmail.com>
2022-12-29 02:04:34 +0100

    ...

--- lib/IR/Print.cc
+++ lib/IR/Print.cc
@@ -1,10 +1,12 @@
 #include "IR/Print.h"
 
 #include <iostream>
+#include <sstream>
 
 #include "Basic/Basic.h"
 #include "IR/CFG.h"
 #include "IR/Module.h"
+#include "Basic/PrintUtil.h"
 
 using namespace scatha;
 using namespace ir;
@@ -12,7 +14,7 @@ using namespace ir;
 namespace {
 
 struct PrintCtx {
-    explicit PrintCtx(std::ostream& str): str(str) {}
+    explicit PrintCtx(std::ostream& str): str(str), indent(2) {}
 
     void dispatch(Value const&);
 
@@ -34,6 +36,7 @@ struct PrintCtx {
     std::string toString(Value const&);
 
     std::ostream& str;
+    Indenter indent;
 };
 
 } // namespace
@@ -44,10 +47,15 @@ void ir::print(Module const& program) {
 
 void ir::print(Module const& program, std::ostream& str) {
     PrintCtx ctx(str);
-
     for (auto& function: program.functions()) {
         ctx.dispatch(function);
     }
+}
+
+std::ostream& ir::operator<<(std::ostream& ostream, Instruction const& inst) {
+    PrintCtx ctx(ostream);
+    ctx.dispatch(inst);
+    return ostream;
 }
 
 void PrintCtx::dispatch(Value const& value) {
@@ -60,77 +68,74 @@ void PrintCtx::print(Function const& function) {
         str << (first ? (void)(first = false), "" : ", ") << param.type()->name() << " %" << param.name();
     }
     str << ") {\n";
+    indent.increase();
     for (auto& bb: function.basicBlocks()) {
         dispatch(bb);
     }
+    indent.decrease();
     str << "}\n";
 }
 
 void PrintCtx::print(BasicBlock const& bb) {
-    str << "  %" << bb.name() << ":\n";
+    str << indent << "%" << bb.name() << ":\n";
+    indent.increase();
     for (auto& instruction: bb.instructions) {
         dispatch(instruction);
+        str << "\n";
     }
+    indent.decrease();
 }
 
 void PrintCtx::print(Instruction const& inst) {
-    str << "    <<" << inst.nodeType() << ">>\n";
+    str << indent << "<<" << inst.nodeType() << ">>\n";
 }
 
 void PrintCtx::print(Alloca const& alloc) {
-    str << "    %" << alloc.name() << " = alloca " << alloc.allocatedType()->name() << "\n";
+    str << indent << "%" << alloc.name() << " = alloca " << alloc.allocatedType()->name();
 }
 
 void PrintCtx::print(Load const& load) {
-    str << "    %" << load.name() << " = load " << load.type()->name() << ", ptr %" << load.operand()->name() << "\n";
+    str << indent << "%" << load.name() << " = load " << load.type()->name() << ", ptr %" << load.operand()->name();
 }
 
 void PrintCtx::print(Store const& store) {
-    str << "    "
-        << "store ";
+    str << indent << "store ";
     str << store.lhs()->type()->name() << " " << toString(*store.lhs()) << ", ";
-    str << store.rhs()->type()->name() << " " << toString(*store.rhs()) << "\n";
+    str << store.rhs()->type()->name() << " " << toString(*store.rhs());
 }
 
 void PrintCtx::print(CompareInst const& cmp) {
-    str << "    %" << cmp.name() << " = cmp " << cmp.operation() << " ";
+    str << indent << "%" << cmp.name() << " = cmp " << cmp.operation() << " ";
     str << cmp.lhs()->type()->name() << " " << toString(*cmp.lhs()) << ", ";
-    str << cmp.rhs()->type()->name() << " " << toString(*cmp.rhs()) << "\n";
+    str << cmp.rhs()->type()->name() << " " << toString(*cmp.rhs());
 }
 
 void PrintCtx::print(ArithmeticInst const& arith) {
-    str << "    %" << arith.name() << " = " << arith.operation() << " " << arith.lhs()->type()->name() << " ";
+    str << indent << "%" << arith.name() << " = " << arith.operation() << " " << arith.lhs()->type()->name() << " ";
     str << toString(*arith.lhs()) << ", ";
-    str << toString(*arith.rhs()) << "\n";
+    str << toString(*arith.rhs());
 }
 
 void PrintCtx::print(Goto const& gt) {
-    str << "    "
-        << "goto "
-        << "label %" << gt.target()->name() << "\n";
+    str << indent << "goto " << "label %" << gt.target()->name();
 }
 
 void PrintCtx::print(Branch const& br) {
-    str << "    "
+    str << indent
         << "branch " << br.condition()->type()->name() << " " << toString(*br.condition()) << ", ";
-    str << "label %" << br.ifTarget()->name() << ", ";
-    str << "label %" << br.elseTarget()->name() << "\n";
+    str << "label %" << br.thenTarget()->name() << ", ";
+    str << "label %" << br.elseTarget()->name();
 }
 
 void PrintCtx::print(Return const& ret) {
-    str << "    "
-        << "return ";
-
-    if (!ret.value()) {
-        str << "\n";
-    }
-    else {
-        str << ret.value()->type()->name() << " " << toString(*ret.value()) << "\n";
+    str << indent << "return ";
+    if (ret.value()) {
+        str << ret.value()->type()->name() << " " << toString(*ret.value());
     }
 }
 
 void PrintCtx::print(FunctionCall const& call) {
-    str << "    ";
+    str << indent;
     if (!call.name().empty()) {
         str << "%" << call.name() << " = ";
     }
@@ -138,21 +143,20 @@ void PrintCtx::print(FunctionCall const& call) {
     for (auto& arg: call.arguments()) {
         str << ", " << arg->type()->name() << " " << toString(*arg);
     }
-    str << "\n";
 }
 
 void PrintCtx::print(Phi const& phi) {
-    str << "    %" << phi.name() << " = phi " << phi.type()->name();
+    str << indent << "%" << phi.name() << " = phi " << phi.type()->name();
     for (auto& [pred, value]: phi.arguments) {
         str << ", [label %" << pred->name() << ", " << toString(*value) << "]";
     }
-    str << "\n";
 }
 
 std::string PrintCtx::toString(Value const& value) {
-    return visit(value,
-                 utl::overload{
-                     [&](Value const& value) -> std::string { return utl::strcat("%", value.name()); },
-                     [&](IntegralConstant const& value) -> std::string { return value.value().toString(); },
-                 });
+    // clang-format off
+    return visit(value, utl::overload{
+        [&](Value const& value) -> std::string { return utl::strcat("%", value.name()); },
+        [&](IntegralConstant const& value) -> std::string { return value.value().toString(); },
+    });
+    // clang-format on
 }
