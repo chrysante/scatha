fa526e67ce3b77d9769e5d5bafcf4a33966e6af7
chrysante <gruenlilie@protonmail.com>
2023-01-07 00:45:36 +0100

    ...

--- scripts/copy-public-headers-unix.sh
+++ scripts/copy-public-headers-unix.sh
@@ -2,10 +2,7 @@ rm -r "include/scatha"
 
 while IFS="" read -r file || [ -n "$file" ]
 do
-    #echo "Processing $file"
     export dir=${file%/*}
     mkdir -p "include/scatha/$dir"
     cp "lib/$file" "include/scatha/$file"
-    
-    #echo "\tin directory $dir"
 done < lib/public-headers
