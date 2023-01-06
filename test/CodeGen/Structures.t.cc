98ce10c48bc88a7e2481b1074a91189a2d9ec253
chrysante <gruenlilie@protonmail.com>
2023-01-06 17:44:36 +0100

    Complicated test case

--- test/CodeGen/Structures.t.cc
+++ test/CodeGen/Structures.t.cc
@@ -132,6 +132,11 @@ struct X {
     var c: bool;
     var d: bool;
     var a: int;
+    var y: Y;
+}
+struct Y {
+    var i: int;
+    var f: float;
 }
 fn makeX() -> X {
     var x: X;
@@ -139,13 +144,19 @@ fn makeX() -> X {
     x.b = false;
     x.c = true;
     x.d = false;
+    x.y = makeY();
     return forward(x);
 }
-fn forward(x: X) -> X {
-    return x;
+fn makeY() -> Y {
+    var y: Y;
+    y.i = -1;
+    y.f = 0.5;
+    return y;
 }
+fn forward(x: X) -> X { return x; }
+fn forward(y: Y) -> Y { return y; }
 fn main() -> int {
-    if forward(forward(makeX())).c {
+    if forward(makeX().y).f == 0.5 {
         return 5;
     }
     return 6;
