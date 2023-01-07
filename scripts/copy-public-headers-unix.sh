481b897238345c0c924dc34a7f74cac1b363b53e
chrysante <gruenlilie@protonmail.com>
2023-01-07 01:16:50 +0100

    ignore blank lines in script

--- scripts/copy-public-headers-unix.sh
+++ scripts/copy-public-headers-unix.sh
@@ -2,6 +2,9 @@ rm -r "include/scatha"
 
 while IFS="" read -r file || [ -n "$file" ]
 do
+    if [ -z "$file" ]; then
+        continue
+    fi
     export dir=${file%/*}
     mkdir -p "include/scatha/$dir"
     cp "lib/$file" "include/scatha/$file"
