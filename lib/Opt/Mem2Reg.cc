f6f331f6abcc62f1650b5af0d435433e11840896
chrysante <gruenlilie@protonmail.com>
2023-01-16 14:34:03 +0100

    Improved mem2reg algorithm

--- lib/Opt/Mem2Reg.cc
+++ lib/Opt/Mem2Reg.cc
@@ -29,14 +29,16 @@ struct Mem2RegContext {
 
     Value* search(BasicBlock*, size_t depth, size_t bifurkations);
 
+    Value* combinePredecessors(BasicBlock*, size_t depth, size_t bifurkations);
+    
     Value* findReplacement(Value* value);
 
-    void evictIfDead(Store* store);
+    void evict(Instruction* inst);
 
     bool isDead(Store const* store);
 
-    void evictIfDead(Alloca* inst);
-
+    static bool isUnused(Instruction const* inst);
+    
     void gather();
 
     void setCurrentLoad(Load* load) { _currentLoad = load; }
@@ -46,17 +48,29 @@ struct Mem2RegContext {
     ir::Context& irCtx;
     Function& function;
 
+    /// LoadAndStoreMap
+    using LoadAndStoreKey = std::pair<BasicBlock*, Value*>;
+    struct LoadAndStoreKeyHash {
+        size_t operator()(LoadAndStoreKey const& key) const { return std::hash<BasicBlock*>{}(key.first); }
+    };
+    struct LoadAndStoreKeyEqual {
+        bool operator()(LoadAndStoreKey const& a, LoadAndStoreKey const& b) const {
+            return a.first == b.first && addressEqual(a.second, b.second);
+        }
+    };
+    using LoadAndStoreMap = utl::hashmap<LoadAndStoreKey, utl::small_vector<Instruction*>, LoadAndStoreKeyHash, LoadAndStoreKeyEqual>;
+    
     /// Maps pairs of basic blocks and addresses to lists of load and store instructions from and to that address in
     /// that basic block.
-    utl::hashmap<std::pair<BasicBlock*, Value*>, utl::small_vector<Instruction*>> loadsAndStores;
+    LoadAndStoreMap loadsAndStores;
     /// Maps evicted load instructions to their respective replacement values.
     utl::hashmap<Load const*, Value*> loadReplacementMap;
     /// List of all load instructions in the function.
     utl::small_vector<Load*> loads;
     /// List of all store instructions in the function.
     utl::small_vector<Store*> stores;
-    /// List of all alloca instructions in the function.
-    utl::hashset<Alloca*> allocas;
+    /// List of all other memory instructions in the function. For now that is \p alloca 's and \p gep 's
+    utl::small_vector<Instruction*> otherMemInstructions;
 
     Load* _currentLoad = nullptr;
 };
@@ -77,10 +91,14 @@ void Mem2RegContext::run() {
         promote(load);
     }
     for (auto* const store: stores) {
-        evictIfDead(store);
+        if (isDead(store)) {
+            evict(store);
+        }
     }
-    for (auto* const inst: allocas) {
-        evictIfDead(inst);
+    for (auto* const inst: otherMemInstructions) {
+        if (isUnused(inst)) {
+            evict(inst);
+        }
     }
 }
 
@@ -98,19 +116,6 @@ bool Mem2RegContext::promote(Load* load) {
     currentLoad()->clearOperands();
     setCurrentLoad(nullptr);
     return true;
-}
-
-static Phi* findPhiWithArgs(BasicBlock* basicBlock, std::span<PhiMapping const> args) {
-    Phi* phi = nullptr;
-    for (auto itr = basicBlock->instructions.begin();
-         itr != basicBlock->instructions.end() && (phi = dyncast<Phi*>(itr.to_address())) != nullptr;
-         ++itr)
-    {
-        if (compareEqual(phi, args)) {
-            return phi;
-        }
-    }
-    return nullptr;
 }
 
 Value* Mem2RegContext::search(BasicBlock* basicBlock, size_t depth, size_t bifurkations) {
@@ -135,39 +140,55 @@ Value* Mem2RegContext::search(BasicBlock* basicBlock, size_t depth, size_t bifur
     switch (basicBlock->predecessors.size()) {
     case 0: return nullptr;
     case 1: return search(basicBlock->predecessors.front(), depth + 1, bifurkations);
-    default:
-        utl::small_vector<PhiMapping> phiArgs;
-        size_t const predCount = basicBlock->predecessors.size();
-        phiArgs.reserve(predCount);
-        size_t numPredsEqualToSelf = 0;
-        Value* valueUnequalToSelf  = nullptr;
-        for (auto* pred: basicBlock->predecessors) {
-            PhiMapping arg{ pred, search(pred, depth + 1, bifurkations + 1) };
-            SC_ASSERT(arg.value,
-                      "This probably just means we can't promote or have to insert a load into pred, but we figure it "
-                      "out later.");
-            phiArgs.push_back(arg);
-            if (arg.value == currentLoad()) {
-                ++numPredsEqualToSelf;
-            }
-            else {
-                valueUnequalToSelf = arg.value;
-            }
+    default: return combinePredecessors(basicBlock, depth, bifurkations);
+    }
+}
+
+static Phi* findPhiWithArgs(BasicBlock* basicBlock, std::span<PhiMapping const> args) {
+    for (auto& inst: basicBlock->instructions) {
+        Phi* const phi = dyncast<Phi*>(&inst);
+        if (phi == nullptr) {
+            break;
         }
-        SC_ASSERT(numPredsEqualToSelf < predCount,
-                  "How can all predecessors refer to this load? How would we than even reach this basic block?");
-        if (numPredsEqualToSelf == predCount - 1) {
-            return valueUnequalToSelf;
-        }
-        if (auto* phi = findPhiWithArgs(basicBlock, phiArgs)) {
+        if (compareEqual(phi, args)) {
             return phi;
         }
-        std::string name = bifurkations == 0 ? std::string(currentLoad()->name()) :
-                                               irCtx.uniqueName(&function, currentLoad()->name(), ".p", bifurkations);
-        auto* phi        = new Phi(std::move(phiArgs), std::move(name));
-        basicBlock->addInstruction(basicBlock->instructions.begin(), phi);
+    }
+    return nullptr;
+}
+
+Value* Mem2RegContext::combinePredecessors(BasicBlock* basicBlock, size_t depth, size_t bifurkations) {
+    utl::small_vector<PhiMapping> phiArgs;
+    size_t const predCount = basicBlock->predecessors.size();
+    phiArgs.reserve(predCount);
+    size_t numPredsEqualToSelf = 0;
+    Value* valueUnequalToSelf  = nullptr;
+    for (auto* pred: basicBlock->predecessors) {
+        PhiMapping arg{ pred, search(pred, depth + 1, bifurkations + 1) };
+        SC_ASSERT(arg.value,
+                  "This probably just means we can't promote or have to insert a load into pred, but we figure it "
+                  "out later.");
+        phiArgs.push_back(arg);
+        if (arg.value == currentLoad()) {
+            ++numPredsEqualToSelf;
+        }
+        else {
+            valueUnequalToSelf = arg.value;
+        }
+    }
+    SC_ASSERT(numPredsEqualToSelf < predCount,
+              "How can all predecessors refer to this load? How would we than even reach this basic block?");
+    if (numPredsEqualToSelf == predCount - 1) {
+        return valueUnequalToSelf;
+    }
+    if (auto* phi = findPhiWithArgs(basicBlock, phiArgs)) {
         return phi;
     }
+    std::string name = bifurkations == 0 ? std::string(currentLoad()->name()) :
+                                           irCtx.uniqueName(&function, currentLoad()->name(), ".p", bifurkations);
+    auto* phi        = new Phi(std::move(phiArgs), std::move(name));
+    basicBlock->addInstruction(basicBlock->instructions.begin(), phi);
+    return phi;
 }
 
 Value* Mem2RegContext::findReplacement(Value* value) {
@@ -178,12 +199,11 @@ Value* Mem2RegContext::findReplacement(Value* value) {
     return value;
 }
 
-void Mem2RegContext::evictIfDead(Store* store) {
-    if (!isDead(store)) {
-        return;
+void Mem2RegContext::evict(Instruction* inst) {
+    if (auto* user = dyncast<User*>(inst)) {
+        user->clearOperands();
     }
-    store->parent()->instructions.erase(store);
-    store->clearOperands();
+    inst->parent()->instructions.erase(inst);
 }
 
 static std::optional<std::tuple<Value const*, size_t, size_t>> getConstantBaseAndOffset(Value const* addr) {
@@ -225,7 +245,7 @@ static boost::tribool testAddressOverlap(Value const* a, Value const* b) {
 
 bool Mem2RegContext::isDead(Store const* store) {
     Value const* const destAddress = store->dest();
-    if (!allocas.contains(destAddress)) {
+    if (!isLocalMemory(destAddress)) {
         /// We can only guarantee that this store is dead if the memory was locally allocated by this function.
         return false;
     }
@@ -241,10 +261,8 @@ bool Mem2RegContext::isDead(Store const* store) {
     return true;
 }
 
-void Mem2RegContext::evictIfDead(Alloca* inst) {
-    if (inst->users().empty()) {
-        inst->parent()->instructions.erase(inst);
-    }
+bool Mem2RegContext::isUnused(Instruction const* inst) {
+    return inst->users().empty();
 }
 
 void Mem2RegContext::gather() {
@@ -260,12 +278,15 @@ void Mem2RegContext::gather() {
                 loadsAndStores[{ store.parent(), store.dest() }].push_back(&store);
             },
             [&](Alloca& inst) {
-                allocas.insert(&inst);
+                otherMemInstructions.push_back(&inst);
+            },
+            [&](GetElementPointer& gep) {
+                otherMemInstructions.push_back(&gep);
             },
             [&](auto&) {},
         }); // clang-format on
     }
-    for ([[maybe_unused]] auto&& [bb, ls]: loadsAndStores) {
+    for ([[maybe_unused]] auto&& [bb_addr, ls]: loadsAndStores) {
         SC_ASSERT(std::is_sorted(ls.begin(),
                                  ls.end(),
                                  [](Instruction const* a, Instruction const* b) { return preceeds(a, b); }),
