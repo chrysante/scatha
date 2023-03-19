699d693759b6b464675ae5f935c76bb3933c8b70
chrysante <gruenlilie@protonmail.com>
2023-03-20 00:35:18 +0100

    Added more boilerplate

--- lib/Opt/SROA.cc
+++ lib/Opt/SROA.cc
@@ -1,9 +1,38 @@
 #include "Opt/SROA.h"
 
+#include "IR/Context.h"
+#include "IR/CFG.h"
+
 using namespace scatha;
 using namespace ir;
 using namespace opt;
 
-bool opt::sroa(ir::Context& context, ir::Function& function) {
+namespace {
+
+struct SROAContext {
+    SROAContext(ir::Context& irCtx, ir::Function& function):
+        irCtx(irCtx), function(function) {}
+    
+    bool run();
+    
+    ir::Context& irCtx;
+    ir::Function& function;
+};
+
+} // namespace
+
+bool opt::sroa(ir::Context& irCtx, ir::Function& function) {
+    SROAContext ctx(irCtx, function);
+    return ctx.run();
+}
+
+bool SROAContext::run() {
+    for (auto& inst: function.entry()) {
+        auto* address = dyncast<Alloca*>(&inst);
+        if (!address) {
+            continue;
+        }
+        
+    }
     SC_DEBUGFAIL();
 }
