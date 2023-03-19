a941e4abe7067aeb8d6bb71bf289346de51e3d4c
chrysante <gruenlilie@protonmail.com>
2023-03-19 17:31:33 +0100

    SROA boilerplate

---
+++ lib/Opt/SROA.cc
@@ -0,0 +1,9 @@
+#include "Opt/SROA.h"
+
+using namespace scatha;
+using namespace ir;
+using namespace opt;
+
+bool opt::sroa(ir::Context& context, ir::Function& function) {
+    SC_DEBUGFAIL();
+}
---
+++ lib/Opt/SROA.h
@@ -0,0 +1,21 @@
+#ifndef SCATHA_OPT_SROA_H_
+#define SCATHA_OPT_SROA_H_
+
+#include "Basic/Basic.h"
+
+namespace scatha::ir {
+
+class Context;
+class Function;
+
+} // namespace scatha::ir
+
+namespace scatha::opt {
+
+/// Perform scalar replacement of aggregates on \p function
+/// \Returns True iff \p function was modified in the pass.
+SCATHA(API) bool sroa(ir::Context& context, ir::Function& function);
+
+} // namespace scatha::opt
+
+#endif // SCATHA_OPT_SROA_H_
