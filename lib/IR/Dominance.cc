0feb0070bdb1d2a11e083b2b556741ac671aba65
chrysante <gruenlilie@protonmail.com>
2023-03-25 12:03:08 +0100

    Added post dominance

--- lib/IR/Dominance.cc
+++ lib/IR/Dominance.cc
@@ -1,5 +1,6 @@
 #include "IR/Dominance.h"
 
+#include <array>
 #include <iostream>
 
 #include <range/v3/numeric.hpp>
@@ -45,6 +46,14 @@ static utl::hashset<BasicBlock*> intersect(auto&& range) {
     });
 }
 
+static utl::small_vector<BasicBlock*> exitNodes(Function& function) {
+    return function | ranges::views::filter([](auto& bb) {
+               return isa<Return>(bb.terminator());
+           }) |
+           ranges::views::transform([](auto& bb) { return &bb; }) |
+           ranges::to<utl::small_vector<BasicBlock*>>;
+}
+
 namespace {
 
 struct PrintCtx {
@@ -82,6 +91,14 @@ DominanceInfo DominanceInfo::compute(Function& function) {
     return result;
 }
 
+DominanceInfo DominanceInfo::computePost(Function& function) {
+    DominanceInfo result;
+    result._domMap   = computePostDomSets(function);
+    result._domTree  = computePostDomTree(function, result._domMap);
+    result._domFront = computePostDomFronts(function, result._domTree);
+    return result;
+}
+
 utl::hashset<BasicBlock*> const& DominanceInfo::domSet(
     ir::BasicBlock const* basicBlock) const {
     auto itr = _domMap.find(basicBlock);
@@ -96,7 +113,12 @@ std::span<BasicBlock* const> DominanceInfo::domFront(
     return itr->second;
 }
 
-DominanceInfo::DomMap DominanceInfo::computeDomSets(Function& function) {
+DominanceInfo::DomMap DominanceInfo::computeDomSetsImpl(
+    Function& function,
+    std::span<BasicBlock* const> entries,
+    auto predecessors,
+    auto successors) {
+    /// https://pages.cs.wisc.edu/~fischer/cs701.f07/lectures/Lecture20.pdf
     auto const nodeSet = [&] {
         utl::hashset<BasicBlock*> res;
         for (auto& bb: function) {
@@ -111,20 +133,23 @@ DominanceInfo::DomMap DominanceInfo::computeDomSets(Function& function) {
         }
         return res;
     }();
-    utl::hashset<BasicBlock*> worklist = { &function.entry() };
+    SC_ASSERT(
+        !entries.empty(),
+        "Handle post-dom case without exit notes outside of this function");
+    auto worklist = entries | ranges::to<utl::hashset<BasicBlock*>>;
     while (!worklist.empty()) {
         auto* bb = *worklist.begin();
         worklist.erase(worklist.begin());
         utl::hashset<BasicBlock*> newDomSet = { bb };
         auto predDomSets =
-            bb->predecessors() |
+            predecessors(bb) |
             ranges::views::transform(
                 [&](auto* pred) -> auto const& { return domSets[pred]; });
         merge(newDomSet, intersect(predDomSets));
         auto& oldDomSet = domSets[bb];
         if (newDomSet != oldDomSet) {
             oldDomSet = newDomSet;
-            for (auto* succ: bb->successors()) {
+            for (auto* succ: successors(bb)) {
                 worklist.insert(succ);
             }
         }
@@ -132,25 +157,55 @@ DominanceInfo::DomMap DominanceInfo::computeDomSets(Function& function) {
     return domSets;
 }
 
-DomTree DominanceInfo::computeDomTree(Function& function,
-                                      DomMap const& domSets) {
+DominanceInfo::DomMap DominanceInfo::computeDomSets(Function& function) {
+    return computeDomSetsImpl(
+        function,
+        std::array{ &function.entry() },
+        [](BasicBlock* bb) { return bb->predecessors(); },
+        [](BasicBlock* bb) { return bb->successors(); });
+}
+
+DominanceInfo::DomMap DominanceInfo::computePostDomSets(Function& function) {
+    /// Same as `computeDomSets` but with reversed edges.
+    auto exits = ::exitNodes(function);
+    if (exits.empty()) {
+        return {};
+    }
+    return computeDomSetsImpl(
+        function,
+        exits,
+        [](BasicBlock* bb) { return bb->successors(); },
+        [](BasicBlock* bb) { return bb->predecessors(); });
+}
+
+DomTree DominanceInfo::computeDomTreeImpl(ir::Function& function,
+                                          DomMap const& domSets,
+                                          BasicBlock* entry,
+                                          std::span<BasicBlock* const> exits,
+                                          auto predecessors) {
     DomTree result;
-    auto const nodeSet = [&] {
-        utl::hashset<BasicBlock*> res;
-        for (auto& bb: function) {
-            res.insert(&bb);
-        }
-        return res;
-    }();
+    auto const nodeSet =
+        function | ranges::views::transform([](auto& bb) { return &bb; }) |
+        ranges::to<utl::hashset<BasicBlock*>>;
     result._nodes = nodeSet | ranges::views::transform([](BasicBlock* bb) {
                         return DomTree::Node{ bb };
                     }) |
                     ranges::to<DomTree::NodeSet>;
-    result._root = &result.findMut(&function.entry());
+    if (entry) {
+        result._root = &result.findMut(entry);
+    }
+    else {
+        auto& constRoot = *result._nodes.insert(DomTree::Node(nullptr)).first;
+        result._root    = const_cast<DomTree::Node*>(&constRoot);
+        for (auto* exit: exits) {
+            result._root->addChild(&result.findMut(exit));
+        }
+    }
     for (auto& start: result._nodes) {
         auto const& domSet = domSets.find(start.basicBlock())->second;
-        utl::hashset<DomTree::Node*> visited;
-        auto findParent = [&](DomTree::Node& node,
+        utl::hashset<DomTree::Node*> visited = { const_cast<DomTree::Node*>(
+            &start) };
+        auto findParent                      = [&](DomTree::Node& node,
                               auto& findParent) -> DomTree::Node* {
             if (visited.contains(&node)) {
                 return nullptr;
@@ -159,7 +214,7 @@ DomTree DominanceInfo::computeDomTree(Function& function,
             if (domSet.contains(node.basicBlock())) {
                 return &node;
             }
-            for (auto* pred: node.basicBlock()->predecessors()) {
+            for (auto* pred: predecessors(node.basicBlock())) {
                 auto& predNode = result.findMut(pred);
                 if (auto res = findParent(predNode, findParent)) {
                     return res;
@@ -167,21 +222,52 @@ DomTree DominanceInfo::computeDomTree(Function& function,
             }
             return nullptr;
         };
-        for (auto* pred: start.basicBlock()->predecessors()) {
+        for (auto* pred: predecessors(start.basicBlock())) {
             auto& predNode = result.findMut(pred);
-            if (auto* res = findParent(predNode, findParent)) {
-                auto* mutStart = const_cast<DomTree::Node*>(&start);
-                res->addChild(mutStart);
-                mutStart->setParent(res);
-                break;
+            auto* res      = findParent(predNode, findParent);
+            if (!res) {
+                continue;
             }
+            auto* mutStart = const_cast<DomTree::Node*>(&start);
+            res->addChild(mutStart);
+            mutStart->setParent(res);
+            break;
         }
     }
     return result;
 }
 
-DominanceInfo::DomFrontMap DominanceInfo::computeDomFronts(
-    Function& function, DomTree const& domTree) {
+DomTree DominanceInfo::computeDomTree(Function& function,
+                                      DomMap const& domSets) {
+    return computeDomTreeImpl(function,
+                              domSets,
+                              &function.entry(),
+                              {},
+                              [](BasicBlock* bb) {
+        return bb->predecessors();
+    });
+}
+
+DomTree DominanceInfo::computePostDomTree(Function& function,
+                                          DomMap const& postDomSets) {
+    auto exits = ::exitNodes(function);
+    /// Can't compute post-dominator tree for function without an exit node.
+    if (exits.empty()) {
+        return {};
+    }
+    auto* exitNode = exits.size() == 1 ? exits.front() : nullptr;
+    return computeDomTreeImpl(function,
+                              postDomSets,
+                              exitNode,
+                              exits,
+                              [](BasicBlock* bb) { return bb->successors(); });
+}
+
+DominanceInfo::DomFrontMap DominanceInfo::computeDomFrontsImpl(
+    ir::Function& function, DomTree const& domTree, auto successors) {
+    if (domTree.empty()) {
+        return {};
+    }
     struct DFContext {
         DFContext(Function& function, DomTree const& domTree, DomFrontMap& df):
             function(function), domTree(domTree), df(df) {}
@@ -193,7 +279,8 @@ DominanceInfo::DomFrontMap DominanceInfo::computeDomFronts(
             auto* u   = uNode.basicBlock();
             auto& dfU = df[u];
             // DF_local
-            for (BasicBlock* v: u->successors()) {
+            auto succ = decltype(successors){};
+            for (BasicBlock* v: succ(u)) {
                 if (domTree.idom(v) != u) {
                     dfU.push_back(v);
                 }
@@ -216,4 +303,18 @@ DominanceInfo::DomFrontMap DominanceInfo::computeDomFronts(
     DFContext ctx(function, domTree, result);
     ctx.compute(domTree.root());
     return result;
+}
+
+DominanceInfo::DomFrontMap DominanceInfo::computeDomFronts(
+    Function& function, DomTree const& domTree) {
+    return computeDomFrontsImpl(function, domTree, [](BasicBlock* bb) {
+        return bb->successors();
+    });
+}
+
+DominanceInfo::DomFrontMap DominanceInfo::computePostDomFronts(
+    Function& function, DomTree const& postDomTree) {
+    return computeDomFrontsImpl(function, postDomTree, [](BasicBlock* bb) {
+        return bb->predecessors();
+    });
 }
--- lib/IR/Dominance.h
+++ lib/IR/Dominance.h
@@ -55,6 +55,8 @@ public:
         return (*this)[block].parent().basicBlock();
     }
 
+    bool empty() const { return _nodes.empty(); }
+
 private:
     friend class DominanceInfo;
 
@@ -86,11 +88,12 @@ public:
         utl::hashmap<ir::BasicBlock*, utl::small_vector<ir::BasicBlock*>>;
 
     /// Compute the dominator sets of the basic blocks in \p function
-    /// I.e. for each basic block `B` the set of basic blocks the dominate `B`.
+    /// I.e. for each basic block `B` the set of basic blocks that dominate `B`.
     static DomMap computeDomSets(ir::Function& function);
 
     /// Compute the dominator tree of \p function
-    static DomTree computeDomTree(ir::Function& function, DomMap const& domMap);
+    static DomTree computeDomTree(ir::Function& function,
+                                  DomMap const& domSets);
 
     /// Compute the dominance frontiers of the basic blocks in \p function
     static DomFrontMap computeDomFronts(ir::Function& function,
@@ -100,20 +103,62 @@ public:
     /// Computes dominance sets, dominator tree and dominance frontiers.
     static DominanceInfo compute(ir::Function& function);
 
-    /// \returns a reference to the set of basic blocks dominated by \p
-    /// basicBlock
+    /// Compute the post-dominator sets of the basic blocks in \p function
+    /// I.e. for each basic block `B` the set of basic blocks that are dominated
+    /// by `B`.
+    static DomMap computePostDomSets(ir::Function& function);
+
+    /// Compute the post-dominator tree of \p function
+    ///
+    /// If \p function has no exit nodes, than the post-dominator tree will be
+    /// empty. If \p function has more than one exit node, than the root of the
+    /// post-dominator tree will not correspond to a basic block,  but instead
+    /// be a 'virtual' root node that has the exit nodes as its children.
+    static DomTree computePostDomTree(ir::Function& function,
+                                      DomMap const& postDomSets);
+
+    /// Compute the post-dominance frontiers of the basic blocks in \p function
+    static DomFrontMap computePostDomFronts(ir::Function& function,
+                                            DomTree const& postDomTree);
+
+    /// Compute post-dominance information of \p function
+    /// Computes post-dominance sets, post-dominator tree and post-dominance
+    /// frontiers.
+    static DominanceInfo computePost(ir::Function& function);
+
+    /// \returns a reference to the set of basic blocks dominated (or
+    /// post-dominated) by \p basicBlock
     utl::hashset<ir::BasicBlock*> const& domSet(
         ir::BasicBlock const* basicBlock) const;
 
-    /// \returns the dominator tree
+    /// \returns the dominator (or post-dominator) tree.
     DomTree const& domTree() const { return _domTree; }
 
-    /// \returns a view of the dominance frontier of \p basicBlock
+    /// \returns a view of the dominance (or post-dominance) frontier of \p
+    /// basicBlock
     std::span<ir::BasicBlock* const> domFront(
         ir::BasicBlock const* basicBlock) const;
 
 private:
     friend struct DFContext;
+
+    static DomMap computeDomSetsImpl(
+        ir::Function& function,
+        /// Shall only have more than one element when used to compute post-dom
+        /// sets for function with multiple exits
+        std::span<BasicBlock* const> entries,
+        auto preds,
+        auto succs);
+    static DomTree computeDomTreeImpl(
+        ir::Function& function,
+        DomMap const& domMap,
+        BasicBlock* entry,
+        /// Only used to compute post-dom tree for function with multiple exits
+        std::span<BasicBlock* const> exits,
+        auto preds);
+    static DomFrontMap computeDomFrontsImpl(ir::Function& function,
+                                            DomTree const& domTree,
+                                            auto succs);
 
 private:
     DomMap _domMap;
