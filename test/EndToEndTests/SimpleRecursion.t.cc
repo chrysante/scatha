db136a96a91e918081bdce27cec1760422297f99
chrysante <gruenlilie@protonmail.com>
2023-03-31 12:48:50 +0200

    Added E2E test case

--- test/EndToEndTests/SimpleRecursion.t.cc
+++ test/EndToEndTests/SimpleRecursion.t.cc
@@ -121,3 +121,49 @@ fn pow(base: int, exponent: int) -> int {
     return base * pow(base *  base, exponent / 2);
 })");
 }
+
+TEST_CASE("Direct tail recursion", "[end-to-end]") {
+    test::checkIRReturns(1, R"(
+struct @X {
+    i64, i64
+}
+
+func i64 @main() {
+  %entry:
+    %q = alloca @X
+    %p = getelementptr inbounds @X, ptr %q, i64 0, 0
+    store ptr %p, i64 1
+    %i = load i64, ptr %p
+    goto label %loopheader
+    
+  %loopheader:
+    %cond = cmp leq i64 1, i64 undef
+    goto label %loopbody
+    
+  %loopbody:
+    %x = phi ptr [label %loopheader: %p], [label %loopbody: undef]
+    branch i1 1, label %exit, label %loopbody
+    
+  %exit:
+   %res = call i64 @get_value, ptr undef, ptr undef, i1 1
+   %s = select i1 1, f64 undef, f64 undef
+   return i64 1
+}
+
+func i64 @get_value(ptr, ptr, i1) {
+  %entry:
+    branch i1 %2, label %then, label %else
+  
+  %then:
+    %cond = lnt i1 %2
+    %res.0 = call i64 @get_value, ptr %1, ptr %0, i1 %cond
+    goto label %ret
+  
+  %else:
+    goto label %ret
+  
+  %ret:
+    %res = phi i64 [label %then: %res.0], [label %else: 1]
+    return i64 %res
+})");
+}
