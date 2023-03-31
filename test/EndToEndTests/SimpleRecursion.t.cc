9581aa90e3eb4f382c41d95e0d3deb1bc5959020
chrysante <gruenlilie@protonmail.com>
2023-03-31 15:40:18 +0200

    Edit test program

--- test/EndToEndTests/SimpleRecursion.t.cc
+++ test/EndToEndTests/SimpleRecursion.t.cc
@@ -125,7 +125,8 @@ fn pow(base: int, exponent: int) -> int {
 TEST_CASE("Direct tail recursion", "[end-to-end]") {
     test::checkIRReturns(1, R"(
 struct @X {
-    i64, i64
+  i64,
+  i64
 }
 
 func i64 @main() {
@@ -135,35 +136,35 @@ func i64 @main() {
     store ptr %p, i64 1
     %i = load i64, ptr %p
     goto label %loopheader
-    
-  %loopheader:
+
+  %loopheader:                # preds: entry
     %cond = cmp leq i64 1, i64 undef
     goto label %loopbody
-    
-  %loopbody:
-    %x = phi ptr [label %loopheader: %p], [label %loopbody: undef]
+
+  %loopbody:                  # preds: loopheader, loopbody
+    %x = phi ptr [label %loopheader : %p], [label %loopbody : undef]
     branch i1 1, label %exit, label %loopbody
-    
-  %exit:
-   %res = call i64 @get_value, ptr undef, ptr undef, i1 1
-   %s = select i1 1, f64 undef, f64 undef
-   return i64 1
+
+  %exit:                      # preds: loopbody
+    %res = call i64 @get_value, ptr undef, ptr undef, i1 1
+    %s = select i1 1, f64 undef, f64 undef
+    return i64 1
 }
 
-func i64 @get_value(ptr, ptr, i1) {
+func i64 @get_value(ptr %0, ptr %1, i1 %2) {
   %entry:
     branch i1 %2, label %then, label %else
-  
-  %then:
+
+  %then:                      # preds: entry
     %cond = lnt i1 %2
     %res.0 = call i64 @get_value, ptr %1, ptr %0, i1 %cond
     goto label %ret
-  
-  %else:
+
+  %else:                      # preds: entry
     goto label %ret
-  
-  %ret:
-    %res = phi i64 [label %then: %res.0], [label %else: 1]
+
+  %ret:                       # preds: then, else
+    %res = phi i64 [label %then : %res.0], [label %else : 1]
     return i64 %res
 })");
 }
