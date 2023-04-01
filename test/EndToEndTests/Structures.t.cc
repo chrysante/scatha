e087e6d1de5e15de9f260499f743b0f289d34904
chrysante <gruenlilie@protonmail.com>
2023-04-01 21:40:36 +0200

    Added array indexing test case

--- test/EndToEndTests/Structures.t.cc
+++ test/EndToEndTests/Structures.t.cc
@@ -166,6 +166,29 @@ struct X {
 })");
 }
 
+TEST_CASE("Array indexing", "[end-to-end][array-access]") {
+    test::checkIRReturns(3, R"(
+func i64 @main() {
+  %entry:
+    %a = alloca i64, i32 5
+    call void @set, ptr %a, i32 2, i64 3
+    %r = call i64 @get, ptr %a, i32 2
+    return i64 %r
+}
+func i64 @get(ptr %data, i32 %index) {
+  %entry:
+    %e = getelementptr inbounds i64, ptr %data, i32 %index
+    %r = load i64, ptr %e
+    return i64 %r
+}
+func void @set(ptr %data, i32 %index, i64 %value) {
+  %entry:
+    %e = getelementptr inbounds i64, ptr %data, i32 %index
+    store ptr %e, i64 %value
+    return
+})");
+}
+
 TEST_CASE("Nested array member access",
           "[end-to-end][member-access][array-access]") {
     test::checkIRReturns(2, R"(
