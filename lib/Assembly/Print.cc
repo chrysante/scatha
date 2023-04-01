bda92c9030d164b7df46135ed205cfbc19c00ae1
chrysante <gruenlilie@protonmail.com>
2023-04-01 18:34:07 +0200

    Improved `Asm::print()` function

--- lib/Assembly/Print.cc
+++ lib/Assembly/Print.cc
@@ -33,10 +33,6 @@ void Asm::print(Block const& block, std::ostream& str) {
     }
 }
 
-std::ostream& Asm::operator<<(std::ostream& str, Instruction const& inst) {
-    return inst.visit([&](auto& inst) -> auto& { return str << inst; });
-}
-
 static constexpr utl::streammanip instName =
     [](std::ostream& str, auto const&... args) {
     int const instNameWidth = 8;
@@ -44,88 +40,92 @@ static constexpr utl::streammanip instName =
         << utl::strcat(args...);
 };
 
-std::ostream& Asm::operator<<(std::ostream& str, MoveInst const& mov) {
-    return str << instName("mov", mov.numBytes()) << " " << mov.dest() << ", "
-               << mov.source();
+static void printImpl(std::ostream& str, MoveInst const& mov) {
+    str << instName("mov", mov.numBytes()) << " " << mov.dest() << ", "
+        << mov.source();
 }
 
-std::ostream& Asm::operator<<(std::ostream& str, CMoveInst const& cmov) {
-    return str << instName(toCMoveInstName(cmov.condition()), cmov.numBytes())
-               << " " << cmov.dest() << ", " << cmov.source();
+static void printImpl(std::ostream& str, CMoveInst const& cmov) {
+    str << instName(toCMoveInstName(cmov.condition()), cmov.numBytes()) << " "
+        << cmov.dest() << ", " << cmov.source();
 }
 
-std::ostream& Asm::operator<<(std::ostream& str,
-                              UnaryArithmeticInst const& inst) {
-    return str << instName(inst.operation()) << " " << inst.operand();
+static void printImpl(std::ostream& str, UnaryArithmeticInst const& inst) {
+    str << instName(inst.operation()) << " " << inst.operand();
 }
 
-std::ostream& Asm::operator<<(std::ostream& str, ArithmeticInst const& inst) {
-    return str << instName(inst.operation()) << " " << inst.dest() << ", "
-               << inst.source();
+static void printImpl(std::ostream& str, ArithmeticInst const& inst) {
+    str << instName(inst.operation()) << " " << inst.dest() << ", "
+        << inst.source();
 }
 
-std::ostream& Asm::operator<<(std::ostream& str, JumpInst const& jmp) {
-    return str << instName(toJumpInstName(jmp.condition())) << " "
-               << jmp.targetLabelID();
+static void printImpl(std::ostream& str, JumpInst const& jmp) {
+    str << instName(toJumpInstName(jmp.condition())) << " "
+        << jmp.targetLabelID();
 }
 
-std::ostream& Asm::operator<<(std::ostream& str, CallInst const& call) {
-    return str << instName("call") << " " << call.functionLabelID() << ", "
-               << call.regPtrOffset();
+static void printImpl(std::ostream& str, CallInst const& call) {
+    str << instName("call") << " " << call.functionLabelID() << ", "
+        << call.regPtrOffset();
 }
 
-std::ostream& Asm::operator<<(std::ostream& str, CallExtInst const& call) {
-    return str << instName("callExt") << " " << call.regPtrOffset() << ", "
-               << call.slot() << ", " << call.index();
+static void printImpl(std::ostream& str, CallExtInst const& call) {
+    str << instName("callExt") << " " << call.regPtrOffset() << ", "
+        << call.slot() << ", " << call.index();
 }
 
-std::ostream& Asm::operator<<(std::ostream& str, ReturnInst const&) {
-    return str << instName("ret");
+static void printImpl(std::ostream& str, ReturnInst const&) {
+    str << instName("ret");
 }
 
-std::ostream& Asm::operator<<(std::ostream& str, TerminateInst const&) {
-    return str << instName("terminate");
+static void printImpl(std::ostream& str, TerminateInst const&) {
+    str << instName("terminate");
 }
 
-std::ostream& Asm::operator<<(std::ostream& str, CompareInst const& cmp) {
+static void printImpl(std::ostream& str, CompareInst const& cmp) {
     // clang-format off
     auto name = UTL_MAP_ENUM(cmp.type(), std::string_view, {
         { Type::Signed,   "scmp" },
         { Type::Unsigned, "ucmp" },
         { Type::Float,    "fcmp" },
     }); // clang-format on
-    return str << instName(name) << " " << cmp.lhs() << ", " << cmp.rhs();
+    str << instName(name) << " " << cmp.lhs() << ", " << cmp.rhs();
 }
 
-std::ostream& Asm::operator<<(std::ostream& str, TestInst const& test) {
-    return str << instName(test.type() == Type::Signed ? "stest" : "utest")
-               << " " << test.operand();
+static void printImpl(std::ostream& str, TestInst const& test) {
+    str << instName(test.type() == Type::Signed ? "stest" : "utest") << " "
+        << test.operand();
 }
 
-std::ostream& Asm::operator<<(std::ostream& str, SetInst const& set) {
-    return str << instName(toSetInstName(set.operation())) << " " << set.dest();
+static void printImpl(std::ostream& str, SetInst const& set) {
+    str << instName(toSetInstName(set.operation())) << " " << set.dest();
 }
 
-std::ostream& Asm::operator<<(std::ostream& str, LIncSPInst const& lincsp) {
-    return str << instName("lincsp") << " " << lincsp.dest() << ", "
-               << lincsp.offset();
+static void printImpl(std::ostream& str, LIncSPInst const& lincsp) {
+    str << instName("lincsp") << " " << lincsp.dest() << ", "
+        << lincsp.offset();
 }
 
-std::ostream& Asm::operator<<(std::ostream& str, Value const& value) {
-    return value.visit([&](auto& value) -> auto& { return str << value; });
+static void printImpl(std::ostream& str, LEAInst const& lea) {
+    str << instName("lea") << " " << lea.dest() << ", " << lea.address();
 }
 
-std::ostream& Asm::operator<<(std::ostream& str, RegisterIndex const& regIdx) {
-    return str << "_R[" << regIdx.value() << "]";
+static void printImpl(std::ostream& str, RegisterIndex const& regIdx) {
+    str << "_R[" << regIdx.value() << "]";
 }
 
-std::ostream& Asm::operator<<(std::ostream& str, MemoryAddress const& addr) {
+static void printImpl(std::ostream& str, MemoryAddress const& addr) {
     str << "*(ptr)_R[" << addr.baseptrRegisterIndex() << "]";
     if (!addr.onlyEvaluatesInnerOffset()) {
         str << " + _R[" << addr.offsetCountRegisterIndex() << "] * "
             << addr.constantOffsetMultiplier();
     }
-    return str << " + " << addr.constantInnerOffset();
+    str << " + " << addr.constantInnerOffset();
+}
+
+std::ostream& Asm::operator<<(std::ostream& str, Instruction const& inst) {
+    inst.visit([&](auto& inst) { printImpl(str, inst); });
+    return str;
 }
 
 namespace {
@@ -138,22 +138,27 @@ struct CoutRestore {
 
 } // namespace
 
-std::ostream& Asm::operator<<(std::ostream& str, Value8 const& value) {
+static void printImpl(std::ostream& str, Value8 const& value) {
     CoutRestore r;
-    return str << "(u8)" << std::hex << value.value();
+    str << "(u8)" << std::hex << value.value();
 }
 
-std::ostream& Asm::operator<<(std::ostream& str, Value16 const& value) {
+static void printImpl(std::ostream& str, Value16 const& value) {
     CoutRestore r;
-    return str << "(u16)" << std::hex << value.value();
+    str << "(u16)" << std::hex << value.value();
 }
 
-std::ostream& Asm::operator<<(std::ostream& str, Value32 const& value) {
+static void printImpl(std::ostream& str, Value32 const& value) {
     CoutRestore r;
-    return str << "(u32)" << std::hex << value.value();
+    str << "(u32)" << std::hex << value.value();
 }
 
-std::ostream& Asm::operator<<(std::ostream& str, Value64 const& value) {
+static void printImpl(std::ostream& str, Value64 const& value) {
     CoutRestore r;
-    return str << "(u64)" << std::hex << value.value();
+    str << "(u64)" << std::hex << value.value();
+}
+
+std::ostream& Asm::operator<<(std::ostream& str, Value const& value) {
+    value.visit([&](auto& value) { printImpl(str, value); });
+    return str;
 }
--- lib/Assembly/Print.h
+++ lib/Assembly/Print.h
@@ -19,32 +19,8 @@ SCATHA_API void print(Block const& block);
 SCATHA_API void print(Block const& block, std::ostream& ostream);
 
 SCATHA_API std::ostream& operator<<(std::ostream& ostream, Instruction const&);
-SCATHA_API std::ostream& operator<<(std::ostream& ostream, MoveInst const&);
-SCATHA_API std::ostream& operator<<(std::ostream& ostream, CMoveInst const&);
-SCATHA_API std::ostream& operator<<(std::ostream& ostream,
-                                    UnaryArithmeticInst const&);
-SCATHA_API std::ostream& operator<<(std::ostream& ostream,
-                                    ArithmeticInst const&);
-SCATHA_API std::ostream& operator<<(std::ostream& ostream, JumpInst const&);
-SCATHA_API std::ostream& operator<<(std::ostream& ostream, CallInst const&);
-SCATHA_API std::ostream& operator<<(std::ostream& ostream, CallExtInst const&);
-SCATHA_API std::ostream& operator<<(std::ostream& ostream, ReturnInst const&);
-SCATHA_API std::ostream& operator<<(std::ostream& ostream,
-                                    TerminateInst const&);
-SCATHA_API std::ostream& operator<<(std::ostream& ostream, CompareInst const&);
-SCATHA_API std::ostream& operator<<(std::ostream& ostream, TestInst const&);
-SCATHA_API std::ostream& operator<<(std::ostream& ostream, SetInst const&);
-SCATHA_API std::ostream& operator<<(std::ostream& ostream, LIncSPInst const&);
 
 SCATHA_API std::ostream& operator<<(std::ostream& ostream, Value const&);
-SCATHA_API std::ostream& operator<<(std::ostream& ostream,
-                                    RegisterIndex const&);
-SCATHA_API std::ostream& operator<<(std::ostream& ostream,
-                                    MemoryAddress const&);
-SCATHA_API std::ostream& operator<<(std::ostream& ostream, Value8 const&);
-SCATHA_API std::ostream& operator<<(std::ostream& ostream, Value16 const&);
-SCATHA_API std::ostream& operator<<(std::ostream& ostream, Value32 const&);
-SCATHA_API std::ostream& operator<<(std::ostream& ostream, Value64 const&);
 
 } // namespace scatha::Asm
 
