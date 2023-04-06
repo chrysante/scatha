0e410c158e534149b6cb3dbc371e327065e1d0b3
chrysante <gruenlilie@protonmail.com>
2023-04-06 15:52:52 +0200

    Added generic (O(n^2)) set class for testing

---
+++ test/Set.h
@@ -0,0 +1,36 @@
+#ifndef TEST_SET_H_
+#define TEST_SET_H_
+
+#include <initializer_list>
+#include <iterator>
+
+namespace test {
+
+template <typename T>
+struct Set {
+    Set(std::initializer_list<T> l): elems(l) {}
+
+    utl::small_vector<T> elems;
+};
+
+template <typename T>
+bool operator==(Set<T> const& s, auto const& rng) {
+    if (std::size(rng) != s.elems.size()) {
+        return false;
+    }
+    for (auto&& x: rng) {
+        for (auto&& y: s.elems) {
+            if (x == y) {
+                goto endLoop;
+            }
+        }
+        return false;
+endLoop:
+        continue;
+    }
+    return true;
+}
+
+} // namespace test
+
+#endif // TEST_SET_H_
