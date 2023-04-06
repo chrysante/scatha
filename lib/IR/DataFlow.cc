fc209d83946fafd1db79c136dac4edeec5984da2
chrysante <gruenlilie@protonmail.com>
2023-04-06 21:17:33 +0200

    Added DataFlow.h/.cc

---
+++ lib/IR/DataFlow.cc
@@ -0,0 +1,129 @@
+#include "IR/DataFlow.h"
+
+#include <range/v3/view.hpp>
+
+#include "IR/CFG.h"
+#include "IR/Loop.h"
+
+using namespace scatha;
+using namespace ir;
+
+template <typename T>
+static void merge(utl::hashset<T>& dest,
+                  auto const& source) {
+    for (auto* BB: source) {
+        dest.insert(BB);
+    }
+}
+
+namespace {
+
+struct LivenessContext {
+    enum Flags {
+        Visited   = 1 << 0,
+        Processed = 1 << 1
+    };
+    
+    LivenessContext(auto& in, auto& out): liveIn(in), liveOut(out) {}
+    
+    void run(Function const& F);
+    
+    void dag(BasicBlock const* BB);
+    
+    void loopTree(LoopNestingForest::Node const* node);
+  
+    utl::hashset<Instruction const*> phiUses(BasicBlock const* BB);
+    
+    utl::hashmap<BasicBlock const*, utl::hashset<Instruction const*>> &liveIn, &liveOut;
+    utl::hashmap<BasicBlock const*, int> flags;
+    utl::hashset<std::pair<BasicBlock const*, BasicBlock const*>> backEdges;
+};
+
+} // namespace
+
+LiveSets ir::computeLiveSets(Function const& F) {
+    LiveSets result;
+    LivenessContext ctx(result.in, result.out);
+    ctx.run(F);
+    return result;
+}
+
+void LivenessContext::run(Function const& F) {
+    dag(&F.entry());
+    auto& LNF = F.getOrComputeLNF();
+    for (auto* root: LNF.roots()) {
+        loopTree(root);
+    }
+}
+
+void LivenessContext::dag(BasicBlock const* BB) {
+    flags[BB] |= Visited;
+    for (auto* succ: BB->successors()) {
+        auto const succFlag = flags[succ];
+        if (succFlag & Visited) {
+            backEdges.insert({ BB, succ });
+            continue;
+        }
+        if (succFlag & Processed) {
+            continue;
+        }
+        dag(succ);
+    }
+    auto live = phiUses(BB);
+    for (auto* succ: BB->successors()) {
+        if (backEdges.contains({ BB, succ })) {
+            continue;
+        }
+        auto liveInSucc = liveIn[succ];
+        for (auto& phi: succ->phiNodes()) {
+            liveInSucc.erase(&phi);
+        }
+        merge(live, liveInSucc);
+    }
+    liveOut[BB] = live;
+    for (auto& inst: *BB | ranges::views::reverse) {
+        live.erase(&inst);
+        for (auto* operand: inst.operands()) {
+            if (auto* instOp = dyncast<Instruction const*>(operand)) {
+                live.insert(instOp);
+            }
+        }
+    }
+    for (auto& phi: BB->phiNodes()) {
+        live.insert(&phi);
+    }
+    liveIn[BB] = std::move(live);
+    flags[BB] |= Processed;
+}
+
+void LivenessContext::loopTree(LoopNestingForest::Node const* node) {
+//    if (!node->isLoopNode()) {
+//        return;
+//    }
+    if (node->children().empty()) {
+        return;
+    }
+    auto* header = node->basicBlock();
+    auto liveLoop = liveIn[header];
+    for (auto& phi: header->phiNodes()) {
+        liveLoop.erase(&phi);
+    }
+    for (auto* child: node->children()) {
+        auto* header = child->basicBlock();
+        merge(liveIn[header], liveLoop);
+        merge(liveOut[header], liveLoop);
+        loopTree(child);
+    }
+}
+
+static constexpr auto take_address = ranges::views::transform([](auto& x) { return &x; });
+
+utl::hashset<Instruction const*> LivenessContext::phiUses(BasicBlock const* BB) {
+    return *BB |
+           ranges::views::filter([](Instruction const& inst) {
+               auto&& users = inst.users();
+               return ranges::find_if(users, [](Instruction const* user) { return isa<Phi>(user); }) != ranges::end(users);
+           }) |
+           take_address |
+           ranges::to<utl::hashset<Instruction const*>>;
+}
---
+++ lib/IR/DataFlow.h
@@ -0,0 +1,42 @@
+#ifndef SCATHA_IR_DATAFLOW_H_
+#define SCATHA_IR_DATAFLOW_H_
+
+#include <utl/hashtable.hpp>
+#include <utl/vector.hpp>
+
+#include "IR/Common.h"
+#include "Basic/Basic.h"
+
+namespace scatha::ir {
+
+class LiveSets;
+
+///
+///
+///
+SCATHA_TESTAPI LiveSets computeLiveSets(Function const& F);
+
+class LiveSets {
+    static auto& getImpl(auto& map, auto* BB) {
+        auto itr = map.find(BB);
+        SC_ASSERT(itr != map.end(), "Not found");
+        return itr->second;
+    }
+    
+public:
+    utl::hashset<Instruction const*> const& liveIn(BasicBlock const* BB) const { return getImpl(in, BB); }
+    
+    utl::hashset<Instruction const*> const& liveOut(BasicBlock const* BB) const { return getImpl(out, BB); }
+
+private:
+    friend LiveSets ir::computeLiveSets(Function const&);
+    
+    LiveSets() = default;
+    
+    utl::hashmap<BasicBlock const*, utl::hashset<Instruction const*>> in, out;
+};
+
+
+} // namespace scatha::ir
+
+#endif // SCATHA_IR_DATAFLOW_H_
