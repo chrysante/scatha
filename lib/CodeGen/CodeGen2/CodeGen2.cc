d714f0259b77f2e7c2ed74005e4b559e8a62f0ba
chrysante <gruenlilie@protonmail.com>
2023-04-10 19:28:04 +0200

    Delete stale codegen replacement

--- lib/CodeGen/CodeGen2/CodeGen2.cc
+++
@@ -1,7 +0,0 @@
-#include "CodeGen/CodeGen2.h"
-
-#include "Assembly/AssemblyStream.h"
-#include "Assembly/Instruction.h"
-#include "IR/CFG.h"
-#include "IR/Context.h"
-#include "IR/Module.h"
--- lib/CodeGen/CodeGen2/RegisterDescriptor.cc
+++
@@ -1,7 +0,0 @@
-#include "CodeGen/CodeGen2/RegisterDescriptor.h"
-
-using namespace scatha;
-using namespace cg;
-
-RegisterDescriptor::RegisterDescriptor(size_t numRegisters):
-    registers(numRegisters) {}
