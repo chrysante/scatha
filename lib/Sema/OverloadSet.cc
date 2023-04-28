998e9970073c8438b993d4d6dcb0f65d33e8f255
chrysante <gruenlilie@protonmail.com>
2023-04-28 09:47:56 +0200

    Improve signature matching

--- lib/Sema/OverloadSet.cc
+++ lib/Sema/OverloadSet.cc
@@ -2,15 +2,42 @@
 
 #include <range/v3/view.hpp>
 
-namespace scatha::sema {
+#include "Sema/QualType.h"
+
+using namespace scatha;
+using namespace sema;
+
+static bool signatureMatch(std::span<QualType const* const> parameterTypes,
+                           std::span<QualType const* const> argumentTypes) {
+    if (parameterTypes.size() != argumentTypes.size()) {
+        return false;
+    }
+    for (auto [param, arg]: ranges::views::zip(parameterTypes, argumentTypes)) {
+        if (param->base() != arg->base()) {
+            return false;
+        }
+        SC_ASSERT(!param->has(TypeQualifiers::ExplicitReference), "");
+        if (param->has(TypeQualifiers::ImplicitReference) &&
+            !arg->has(TypeQualifiers::ExplicitReference))
+        {
+            return false;
+        }
+        if (!param->has(TypeQualifiers::ImplicitReference) &&
+            arg->has(TypeQualifiers::ExplicitReference))
+        {
+            return false;
+        }
+    }
+    return true;
+}
 
 Function const* OverloadSet::find(
     std::span<QualType const* const> argumentTypes) const {
-    auto matches =
-        functions | ranges::views::filter([&](Function* F) {
-            return ranges::equal(argumentTypes, F->signature().argumentTypes());
-        }) |
-        ranges::to<utl::small_vector<Function*>>;
+    auto matches = functions | ranges::views::filter([&](Function* F) {
+                       return signatureMatch(F->signature().argumentTypes(),
+                                             argumentTypes);
+                   }) |
+                   ranges::to<utl::small_vector<Function*>>;
     if (matches.size() == 1) {
         return matches.front();
     }
@@ -26,5 +53,3 @@ std::pair<Function const*, bool> OverloadSet::add(Function* F) {
     }
     return { *itr, success };
 }
-
-} // namespace scatha::sema
