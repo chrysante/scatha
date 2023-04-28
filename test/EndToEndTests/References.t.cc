5a394182223a288b71078da811aaa1f0d12a0098
chrysante <gruenlilie@protonmail.com>
2023-04-28 09:44:18 +0200

    Add reference test case

--- test/EndToEndTests/References.t.cc
+++ test/EndToEndTests/References.t.cc
@@ -15,3 +15,16 @@ fn f(x: &mut int)  {
     x += 1;
 })");
 }
+
+TEST_CASE("Rebind reference", "[end-to-end][references]") {
+    test::checkReturns(2, R"(
+public fn main() -> int {
+    var i = 0;
+    var j = 0;
+    var r = &i;
+    r += 1;
+    r = &j;
+    r += 1;
+    return i + j;
+})");
+}
