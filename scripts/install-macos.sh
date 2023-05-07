26ad33dc6840f81c4b95a55c56532af66ddfae1d
chrysante <gruenlilie@protonmail.com>
2023-05-07 18:42:53 +0200

    Install script for macos

---
+++ scripts/install-macos.sh
@@ -0,0 +1,6 @@
+xcodebuild -project "$1/scatha-c.xcodeproj" -configuration Release
+xcodebuild -project "$1/svm.xcodeproj" -configuration Release
+
+cp "$1/build/bin/Release/scatha-c" "/usr/local/bin"
+cp "$1/build/bin/Release/libscatha.dylib" "/usr/local/bin"
+cp "$1/build/bin/Release/svm" "/usr/local/bin"
