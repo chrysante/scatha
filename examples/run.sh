a59593c910773c4bdc8637b207e29f4b9d3582b3
chrysante <gruenlilie@protonmail.com>
2023-05-15 00:08:48 +0200

    Removed outdated files

--- examples/run.sh
+++
@@ -1,6 +0,0 @@
-./build/bin/Debug/scatha-c -t -f "examples/$1.sc" -o 0 --objdir "examples/$1.sbin"
-./build/bin/Debug/scatha-c -t -f "examples/$1.sc" -o 1 --objdir "examples/$1-o1.sbin"
-./build/bin/Debug/svm -t -f "examples/$1.sbin"
-./build/bin/Debug/svm -t -f "examples/$1-o1.sbin"
-./build/bin/Release/svm -t -f "examples/$1.sbin"
-./build/bin/Release/svm -t -f "examples/$1-o1.sbin"
