590e0cb69d88f68a9ae5a5b503afc8cda7ca8336
chrysante <gruenlilie@protonmail.com>
2023-05-14 21:47:19 +0200

    Updated install script

--- scripts/install-macos.sh
+++ scripts/install-macos.sh
@@ -1,7 +1,10 @@
 premake5 xcode4
 
-xcodebuild -project "$1/scatha-c.xcodeproj" -configuration Release
-xcodebuild -project "$1/svm.xcodeproj" -configuration Release
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+PROJ_DIR="$SCRIPT_DIR/.."
+
+xcodebuild -project "$PROJ_DIR/scatha-c.xcodeproj" -configuration Release
+xcodebuild -project "$PROJ_DIR/svm.xcodeproj" -configuration Release
 
 cp "$1/build/bin/Release/scatha-c" "/usr/local/bin"
 cp "$1/build/bin/Release/libscatha.dylib" "/usr/local/bin"
