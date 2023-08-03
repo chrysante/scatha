44631a6dbe078100158be02823ba4ec33561db54
chrysante <gruenlilie@protonmail.com>
2023-08-03 02:50:14 +0200

    Added recursion test case

--- test/EndToEndTests/SimpleRecursion.t.cc
+++ test/EndToEndTests/SimpleRecursion.t.cc
@@ -203,3 +203,33 @@ public fn main(n: int) -> int {
     return ack(3, 4);
 })");
 }
+
+TEST_CASE("One level of recursion", "[end-to-end]") {
+    test::checkReturns(3, R"(
+struct Expr {
+    var id: int;
+    var lhs: int;
+    var rhs: int;
+}
+fn eval(expr: Expr) -> int {
+    // 0 == Literal
+    if expr.id == 0 {
+        return expr.lhs;
+    }
+    // 1 == Add
+    if expr.id == 1 {
+        return expr.lhs + expr.rhs;
+    }
+    // 2 == Sub
+    expr.id = 1; // Add
+    expr.rhs = -expr.rhs;
+    return eval(expr);
+}
+public fn main(n: int) -> int {
+    var expr: Expr;
+    expr.id = 2;
+    expr.lhs = 5;
+    expr.rhs = 2;
+    return eval(expr);
+})");
+}
