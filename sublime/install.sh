5f83f581e5d924c4d839459a003fbaa8a7cfdbd1
chrysante <gruenlilie@protonmail.com>
2023-08-11 18:46:26 +0200

    Added preliminary ".scir" sublime syntax

--- sublime/install.sh
+++ sublime/install.sh
@@ -1,2 +1,4 @@
-BASEDIR=$(dirname "$0")
-cp $BASEDIR/scatha.sublime-syntax ~/Library/Application\ Support/Sublime\ Text/Packages/User
+BASE_DIR=$(dirname "$0")
+
+cp $BASE_DIR/scatha.sublime-syntax ~/Library/Application\ Support/Sublime\ Text/Packages/User
+cp $BASE_DIR/scir.sublime-syntax ~/Library/Application\ Support/Sublime\ Text/Packages/User
