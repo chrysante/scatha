c8065fe4c377715ea6004b0596ed67e4defc8464
chrysante <gruenlilie@protonmail.com>
2023-08-15 10:02:45 +0200

    Stub for new simplifycfg pass

---
+++ lib/Opt/SimplifyCFG2.cc
@@ -0,0 +1,59 @@
+/// ↑
+///  ```
+///       │
+///       ↓
+///     ┌───┐
+///     │---│
+///     │---│
+///     └───┘
+///       │
+///       ↓
+///     ┌───┐
+///     │---│
+///     │---│
+///     └───┘
+///       │
+///       ↓
+///  ```
+///  ```
+///       │
+///       ↓
+///     ┌───┐
+///     │---│
+///     │---│
+///     │---│
+///     │---│
+///     └───┘
+///       │
+///       ↓
+///  ```
+
+#include "Opt/Passes.h"
+
+#include "IR/CFG.h"
+#include "IR/Context.h"
+#include "Opt/PassRegistry.h"
+
+using namespace scatha;
+using namespace ir;
+using namespace opt;
+
+SC_REGISTER_PASS(opt::simplifyCFG2, "simplifycfg2");
+
+namespace {
+
+struct SCFGContext {
+    Context& ctx;
+    Function& function;
+
+    SCFGContext(Context& ctx, Function& function):
+        ctx(ctx), function(function) {}
+
+    bool run();
+};
+
+} // namespace
+
+bool opt::simplifyCFG2(ir::Context& ctx, ir::Function& function) { SC_UNIMPLEMENTED(); }
+
+bool SCFGContext::run() { SC_UNIMPLEMENTED(); }
