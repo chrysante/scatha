db448cb969864f34ee2dd7fbab415174d75b2520
chrysante <gruenlilie@protonmail.com>
2023-11-21 16:35:01 +0100

    Added "Hello World" pass

--- examples/misc.sc
+++ examples/misc.sc
@@ -1,10 +1,12 @@
 
 struct X {
+    fn get(this) { return this.n; }
+
     var n: int;
 }
 
 fn main() {
     var x = X(1);
     x.n += 2;
-    return x.n;
+    return x.get();
 }
---
+++ lib/Opt/HelloWorld.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include "IR/CFG.h"
+#include "Opt/PassRegistry.h"
+
+using namespace scatha;
+
+static bool helloWorld(ir::Context&, ir::Function& F) {
+    std::cout << "Hello World! The name of the function is: " << F.name()
+              << std::endl;
+    return false;
+}
+
+SC_REGISTER_PASS(helloWorld, "helloworld");
