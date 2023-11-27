3d9fc8a9e26ca4d49f1e1ba3e89f0e8e6098684a
chrysante <gruenlilie@protonmail.com>
2023-11-27 15:42:36 +0100

    Added documentation

--- include/scatha/Common/Allocator.h
+++ include/scatha/Common/Allocator.h
@@ -8,9 +8,14 @@
 
 namespace scatha {
 
+/// "Arena" allocator. Allocation increases a pointer in the current memory
+/// block or allocates a new block. New blocks grow geometrically in size.
+/// Deallocation is a no-op. Memory gets freed when the allocator is destroyed
+/// or when `release()` is called.
 class SCATHA_API MonotonicBufferAllocator {
 public:
-    static constexpr size_t inititalSize = 128;
+    /// Default value for the size of the first allocated block
+    static constexpr size_t InititalSize = 128;
 
 public:
     MonotonicBufferAllocator();
@@ -18,9 +23,16 @@ public:
     MonotonicBufferAllocator(MonotonicBufferAllocator&&) noexcept;
     ~MonotonicBufferAllocator();
     MonotonicBufferAllocator& operator=(MonotonicBufferAllocator&&) noexcept;
-    void* allocate(size_t size, size_t align);
-    void deallocate(void*, size_t, size_t) {}
 
+    /// Allocate \p size number of bytes aligned to boundary specified by \p
+    /// align
+    void* allocate(size_t size, size_t align);
+
+    /// Deallocate \p ptr
+    /// This function is a no-op and only exists for symmetry with `allocate()`
+    void deallocate(void* ptr, size_t, size_t) {}
+
+    /// Releases the buffer chain and dellocates all memory
     void release();
 
 private:
@@ -38,6 +50,9 @@ private:
     u8* end = nullptr;
 };
 
+/// Allocates memory for object of type `T` using the allocator \p alloc and
+/// constructs the object with arguments \p args... \Returns a pointer the the
+/// constructed object
 template <typename T, typename... Args>
     requires requires(Args&&... args) { T{ std::forward<Args>(args)... }; }
 T* allocate(MonotonicBufferAllocator& alloc, Args&&... args) {
@@ -46,12 +61,16 @@ T* allocate(MonotonicBufferAllocator& alloc, Args&&... args) {
     return result;
 }
 
+/// Allocates memory for an array of element type `T` with \p count elements
+/// using the allocator \p alloc Does not construct the elements
 template <typename T>
 T* allocateArrayUninit(MonotonicBufferAllocator& alloc, size_t count) {
     T* result = static_cast<T*>(alloc.allocate(count * sizeof(T), alignof(T)));
     return result;
 }
 
+/// Allocates memory for an array of element type `T` with \p count elements
+/// using the allocator \p alloc and default constructs the elements
 template <typename T, typename Itr>
 std::span<T> allocateArray(MonotonicBufferAllocator& alloc,
                            Itr begin,
@@ -70,7 +89,7 @@ inline void* operator new(size_t size,
 }
 
 namespace scatha::internal {
-// expose it to the header to be able to test it
+/// Exposed in the header to be able to test it
 SCATHA_API u8* alignPointer(u8* ptr, size_t alignment);
 } // namespace scatha::internal
 
