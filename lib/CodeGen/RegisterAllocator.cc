95b2cbad0d4f5b32969c974f8985f70131518545
chrysante <gruenlilie@protonmail.com>
2023-12-03 10:55:51 +0100

    ...

--- lib/CodeGen/RegisterAllocator.cc
+++ lib/CodeGen/RegisterAllocator.cc
@@ -9,7 +9,7 @@ using namespace scatha;
 using namespace cg;
 using namespace mir;
 
-void cg::allocateRegisters(mir::Function& F) {
+void cg::allocateRegisters(Function& F) {
     /// For instructions that are three address instructions in the MIR
     /// but two address instructions in the VM, we issue copies of the
     /// first operand into the destination register and then replace the
@@ -23,7 +23,7 @@ void cg::allocateRegisters(mir::Function& F) {
         SC_ASSERT(dest != operand,
                   "Here we should be still in kind of SSA form");
         auto* copy =
-            new mir::CopyInst(dest, operand, inst.bytewidth(), inst.metadata());
+            new CopyInst(dest, operand, inst.bytewidth(), inst.metadata());
         inst.parent()->insert(&inst, copy);
         inst.setOperandAt(0, dest);
     }
@@ -38,13 +38,13 @@ void cg::allocateRegisters(mir::Function& F) {
               "that we expect to be indexed with [0, numCols)");
     /// Allocate hardware registers
     for (size_t i = 0; i < numCols; ++i) {
-        F.hardwareRegisters().add(new mir::HardwareRegister());
+        F.hardwareRegisters().add(new HardwareRegister());
     }
     /// Replace all virtual registers with the newly allocated hardware
     /// registers
-    utl::hashmap<mir::VirtualRegister*, mir::HardwareRegister*> registerMap;
+    utl::hashmap<VirtualRegister*, HardwareRegister*> registerMap;
     for (auto* node: graph) {
-        auto* vreg = dyncast<mir::VirtualRegister*>(node->reg());
+        auto* vreg = dyncast<VirtualRegister*>(node->reg());
         if (!vreg) {
             continue;
         }
@@ -82,16 +82,16 @@ void cg::allocateRegisters(mir::Function& F) {
             /// We replace copies from constant 0 with self-xor's. This
             /// decreases binary size because two register indices take 2 bytes
             /// to encode vs >2 bytes for the zero literal.
-            if (auto* constant = dyncast<mir::Constant*>(copy->source());
+            if (auto* constant = dyncast<Constant*>(copy->source());
                 constant && constant->value() == 0 && copy->bytewidth() > 2)
             {
                 auto* selfXor =
-                    new mir::ValueArithmeticInst(copy->dest(),
-                                                 copy->dest(),
-                                                 copy->dest(),
-                                                 copy->bytewidth(),
-                                                 mir::ArithmeticOperation::XOr,
-                                                 copy->metadata());
+                    new ValueArithmeticInst(copy->dest(),
+                                            copy->dest(),
+                                            copy->dest(),
+                                            copy->bytewidth(),
+                                            ArithmeticOperation::XOr,
+                                            copy->metadata());
                 BB.insert(itr, selfXor);
                 itr = BB.erase(itr);
                 continue;
@@ -105,11 +105,11 @@ void cg::allocateRegisters(mir::Function& F) {
         /// We make a copy because we modify the live set as we traverse the
         /// block, so we know at each instruction which registers are live.
         auto live = BB.liveOut();
-        utl::small_vector<mir::Instruction*> toErase;
+        utl::small_vector<Instruction*> toErase;
         for (auto& inst: BB | ranges::views::reverse) {
             auto isLive = [&](auto* reg) { return live.contains(reg); };
             bool canErase = !hasSideEffects(&inst) &&
-                            !isa_or_null<mir::CalleeRegister>(inst.dest()) &&
+                            !isa_or_null<CalleeRegister>(inst.dest()) &&
                             ranges::none_of(inst.destRegisters(), isLive);
             if (canErase) {
                 toErase.push_back(&inst);
@@ -123,7 +123,7 @@ void cg::allocateRegisters(mir::Function& F) {
                     live.erase(reg);
                 }
             }
-            for (auto* reg: inst.operands() | Filter<mir::Register>) {
+            for (auto* reg: inst.operands() | Filter<Register>) {
                 live.insert(reg);
             }
         }
@@ -137,11 +137,11 @@ void cg::allocateRegisters(mir::Function& F) {
     size_t const numLocalRegs = F.hardwareRegisters().size();
     static constexpr size_t NumRegsForCallMetadata = 3;
     for (size_t i = 0; i < NumRegsForCallMetadata; ++i) {
-        F.hardwareRegisters().add(new mir::HardwareRegister());
+        F.hardwareRegisters().add(new HardwareRegister());
     }
     /// Then we replace all callee registers with new hardware registers
     for (auto& calleeReg: F.calleeRegisters()) {
-        auto* hReg = new mir::HardwareRegister();
+        auto* hReg = new HardwareRegister();
         F.hardwareRegisters().add(hReg);
         calleeReg.replaceWith(hReg);
     }
