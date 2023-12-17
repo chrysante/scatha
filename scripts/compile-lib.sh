91bf470f421d6f19df36ac1a2d738209d45321fd
chrysante <gruenlilie@protonmail.com>
2023-12-17 19:40:49 +0100

    ...

--- scripts/compile-lib.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-PROJ_DIR="$SCRIPT_DIR/.."
-
-clang++ -std=c++11 -shared -o $PROJ_DIR/examples/testlib/testlib.dylib  $PROJ_DIR/examples/testlib/testlib.cpp
