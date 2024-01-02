a8e661d7a86e51a9eb243893db1632341c8474bc
chrysante <gruenlilie@protonmail.com>
2024-01-02 11:08:04 +0100

    Added stub functions for binary IR serialization

--- lib/IR/BinSerialize.cc
+++ lib/IR/BinSerialize.cc
@@ -1,3 +1,14 @@
-#include "/BinSerialize.h"
+#include "IR/BinSerialize.h"
 
-using namespace scatha::ir;
+#include "Common/Base.h"
+
+using namespace scatha;
+using namespace ir;
+
+void ir::serializeBinary(ir::Module const&, std::ostream&) {
+    SC_UNIMPLEMENTED();
+}
+
+bool ir::deserializeBinary(ir::Context&, ir::Module&, std::istream&) {
+    SC_UNIMPLEMENTED();
+}
--- lib/IR/BinSerialize.h
+++ lib/IR/BinSerialize.h
@@ -1,8 +1,20 @@
 #ifndef SCATHA_IR_BINSERIALIZE_H_
 #define SCATHA_IR_BINSERIALIZE_H_
 
+#include <iosfwd>
+
+#include "IR/Fwd.h"
+
 namespace scatha::ir {
-    
+
+/// Writes a binary representation of the contents of the IR module \p mod to
+/// the ostream \p out
+void serializeBinary(ir::Module const& mod, std::ostream& out);
+
+/// Parses the binary module representation in the istream \p in into the module
+/// \p mod
+bool deserializeBinary(ir::Context& ctx, ir::Module& mod, std::istream& in);
+
 } // namespace scatha::ir
 
 #endif // SCATHA_IR_BINSERIALIZE_H_
