d692fbbc9573415546a61c8db8c8f162a40bdf5e
chrysante <gruenlilie@protonmail.com>
2024-01-02 15:04:00 +0100

    Catch runtime exceptions

--- svm/src/main.cc
+++ svm/src/main.cc
@@ -20,32 +20,32 @@
 using namespace svm;
 
 int main(int argc, char* argv[]) {
-    Options options = parseCLI(argc, argv);
-    std::string progName = options.filepath.stem();
-    VirtualMachine vm;
     try {
+        Options options = parseCLI(argc, argv);
+        std::string progName = options.filepath.stem();
+        VirtualMachine vm;
         auto binary = readBinaryFromFile(options.filepath.string());
         if (binary.empty()) {
             std::cerr << "Failed to run " << progName << ". Binary is empty.\n";
             return -1;
         }
         vm.loadBinary(binary.data());
+
+        /// Setup arguments on the stack
+        auto execArg = setupArguments(vm, options.arguments);
+        /// Excute the program
+        auto const beginTime = std::chrono::high_resolution_clock::now();
+        vm.execute(execArg);
+        auto const endTime = std::chrono::high_resolution_clock::now();
+        u64 const exitCode = vm.getRegister(0);
+        if (options.time) {
+            std::cout << "Execution took "
+                      << utl::format_duration(endTime - beginTime) << "\n";
+        }
+        return static_cast<int>(exitCode);
     }
     catch (std::exception const& e) {
         std::cout << e.what() << std::endl;
         return 1;
     }
-
-    auto execArg = setupArguments(vm, options.arguments);
-
-    /// Excute the program
-    auto const beginTime = std::chrono::high_resolution_clock::now();
-    vm.execute(execArg);
-    auto const endTime = std::chrono::high_resolution_clock::now();
-    u64 const exitCode = vm.getRegister(0);
-    if (options.time) {
-        std::cout << "Execution took "
-                  << utl::format_duration(endTime - beginTime) << "\n";
-    }
-    return static_cast<int>(exitCode);
 }
