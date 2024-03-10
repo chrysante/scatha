481a72ee7f40efec6c491011b22d5fe931d9ddd9
chrysante <gruenlilie@protonmail.com>
2024-03-10 15:28:27 +0100

    Added `Archive` class

--- scatha/lib/Common/FileHandling.cc
+++ scatha/lib/Common/FileHandling.cc
@@ -3,7 +3,12 @@
 #include <cstring>
 #include <stdexcept>
 
+#include <microtar.h>
+#include <range/v3/view.hpp>
 #include <utl/strcat.hpp>
+#include <utl/utility.hpp>
+
+#include "Common/Base.h"
 
 using namespace scatha;
 
@@ -23,3 +28,83 @@ std::fstream scatha::createOutputFile(std::filesystem::path const& path,
     }
     return file;
 }
+
+static Archive::Impl toImpl(mtar_t const& tar) {
+    static_assert(sizeof(Archive::Impl) >= sizeof(mtar_t));
+    Archive::Impl impl;
+    std::memcpy(&impl.data, &tar, sizeof(tar));
+    return impl;
+}
+
+static mtar_t& asMTar(Archive::Impl& impl) {
+    return reinterpret_cast<mtar_t&>(impl.data);
+}
+
+Archive Archive::Open(std::filesystem::path path) {
+    mtar_t tar;
+    mtar_open(&tar, path.c_str(), "r");
+    mtar_header_t header;
+    std::vector<std::string> files;
+    while ((mtar_read_header(&tar, &header)) != MTAR_ENULLRECORD) {
+        files.emplace_back(header.name);
+        mtar_next(&tar);
+    }
+    return Archive(Mode::Read, std::move(files), toImpl(tar));
+}
+
+Archive Archive::Create(std::filesystem::path path) {
+    mtar_t tar;
+    mtar_open(&tar, path.c_str(), "w");
+    return Archive(Mode::Write, {}, toImpl(tar));
+}
+
+void Archive::close() {
+    auto& tar = asMTar(impl);
+    mtar_finalize(&tar);
+    mtar_close(&tar);
+}
+
+std::optional<std::string> Archive::openTextFile(std::string const& name) {
+    return readImpl<std::string>(name);
+}
+
+std::optional<std::vector<unsigned char>> Archive::openBinaryFile(
+    std::string const& name) {
+    return readImpl<std::vector<unsigned char>>(name);
+}
+
+template <typename T>
+std::optional<T> Archive::readImpl(std::string const& name) {
+    SC_EXPECT(mode() == Mode::Read);
+    auto& tar = asMTar(impl);
+    mtar_header_t header;
+    if (mtar_find(&tar, name.c_str(), &header) != MTAR_ESUCCESS) {
+        return std::nullopt;
+    }
+    T result(header.size, 0);
+    mtar_read_data(&tar, result.data(), header.size);
+    return result;
+}
+
+/// # Writing
+
+void Archive::addTextFile(std::string const& name, std::string_view contents) {
+    writeImpl(name, contents.data(), contents.size());
+}
+
+void Archive::addBinaryFile(std::string const& name,
+                            std::span<unsigned char const> contents) {
+    writeImpl(name, contents.data(), contents.size());
+}
+
+void Archive::writeImpl(std::string const& name, void const* data,
+                        size_t size) {
+    SC_EXPECT(mode() == Mode::Write);
+    auto& tar = asMTar(impl);
+    mtar_write_file_header(&tar, name.c_str(),
+                           utl::narrow_cast<unsigned>(size));
+    mtar_write_data(&tar, data, utl::narrow_cast<unsigned>(size));
+}
+
+Archive::Archive(Mode mode, std::vector<std::string> files, Impl impl):
+    _mode(mode), _files(std::move(files)), impl(impl) {}
--- scatha/lib/Common/FileHandling.h
+++ scatha/lib/Common/FileHandling.h
@@ -3,6 +3,10 @@
 
 #include <filesystem>
 #include <fstream>
+#include <optional>
+#include <span>
+#include <string>
+#include <vector>
 
 namespace scatha {
 
@@ -12,6 +16,76 @@ namespace scatha {
 /// created
 std::fstream createOutputFile(std::filesystem::path const& dest,
                               std::ios::openmode flags);
+
+/// Represents a "tar" file containing multiple other files
+class Archive {
+public:
+    ///
+    enum class Mode { Read, Write, Closed };
+
+    /// # Static constructors
+
+    /// Opens "tar" archive at \p path for reading
+    /// \Throws `std::runtime_error` if the file \p name does not exist
+    static Archive Open(std::filesystem::path path);
+
+    /// Create a "tar" archive at \p path for writing
+    static Archive Create(std::filesystem::path path);
+
+    /// # Queries
+
+    /// \Returns the mode with which this archive was created
+    Mode mode() const { return _mode; }
+
+    /// \Returns a view over the files in this archive
+    std::span<std::string const> files() const { return _files; }
+
+    /// # Modifiers
+
+    /// Closes the archive
+    void close();
+
+    ///
+    ~Archive() { close(); }
+
+    /// # Reading
+
+    /// \Returns the contents of the file \p name as a string
+    /// \Pre `mode()` must be `Read`
+    std::optional<std::string> openTextFile(std::string const& name);
+
+    /// \Returns the contents of the file \p name as binary data
+    /// \Pre `mode()` must be `Read`
+    std::optional<std::vector<unsigned char>> openBinaryFile(
+        std::string const& name);
+
+    /// # Writing
+
+    ///
+    /// \Pre `mode()` must be `Write`
+    void addTextFile(std::string const& name, std::string_view contents);
+
+    ///
+    /// \Pre `mode()` must be `Write`
+    void addBinaryFile(std::string const& name,
+                       std::span<unsigned char const> contents);
+
+    struct Impl {
+        alignas(8) char data[64];
+    };
+
+private:
+    explicit Archive(Mode mode, std::vector<std::string> files, Impl impl);
+
+    template <typename T>
+    std::optional<T> readImpl(std::string const& name);
+
+    void writeImpl(std::string const& name, void const* data, size_t size);
+
+    Mode _mode;
+    std::vector<std::string> _files;
+    Impl impl;
+};
 
 } // namespace scatha
 
