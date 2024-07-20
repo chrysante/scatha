f0926b517d523912bc9b38a1bb315631c29250e5
chrysante <gruenlilie@protonmail.com>
2024-07-20 13:13:41 +0200

    Added pointer analysis test cases

--- scatha/test/Opt/PointerAnalysis.t.cc
+++ scatha/test/Opt/PointerAnalysis.t.cc
@@ -6,6 +6,7 @@
 #include "IR/Context.h"
 #include "IR/IRParser.h"
 #include "IR/Module.h"
+#include "Opt/PassTest.h"
 #include "Opt/Passes.h"
 
 using namespace scatha;
@@ -32,7 +33,7 @@ func i1 @F() {
     CHECK(retval->value() == 0);
 }
 
-TEST_CASE("Pointer analysis alloca and builtin.alloc",
+TEST_CASE("Pointer analysis alloca and __builtin_alloc",
           "[opt][pointer-analysis]") {
     auto [ctx, mod] = ir::parse(R"(
 ext func { ptr, i64 } @__builtin_alloc(i64, i64)
@@ -53,4 +54,99 @@ func i1 @F() {
     auto* retval = cast<IntegralConstant const*>(
         cast<Return const*>(entry.terminator())->value());
     CHECK(retval->value() == 0);
+}
+
+TEST_CASE("Compare pointers in same allocation", "[opt][pointer-analysis]") {
+    test::passTest("ptranalysis, instcombine, sroa, instcombine",
+                   R"(
+@array = constant [i64, 2] [i64 0, i64 1]
+  #ptr(align: 8, validsize: 16, provenance: ptr @array, offset: 0, nonnull)
+
+ext func void @__builtin_memcpy(ptr %0, i64 %1, ptr %2, i64 %3)
+
+func i1 @test() {
+  %entry:
+    %i.addr = alloca i64, i32 2
+    call void @__builtin_memcpy, ptr %i.addr, i64 16, ptr @array, i64 16
+    %elem.addr = getelementptr inbounds i64, ptr %i.addr, i64 0
+    %elem.addr.0 = getelementptr inbounds i64, ptr %i.addr, i64 1
+    %eq = ucmp eq ptr %elem.addr, ptr %elem.addr.0
+    return i1 %eq
+})",
+                   R"(
+func i1 @test() {
+  %entry:
+    return i1 0
+})");
+}
+
+TEST_CASE("Compare unknown pointer to non-escaping alloca",
+          "[opt][pointer-analysis]") {
+    test::passTest("ptranalysis, instcombine, sroa",
+                   R"(
+func i1 @main() {
+  %entry:
+    %i.addr = alloca i64, i32 1
+    store ptr %i.addr, i64 0
+    %call.result = call ptr @makePtr
+    %eq = ucmp eq ptr %i.addr, ptr %call.result
+    return i1 %eq
+}
+func ptr @makePtr() {
+  %entry:
+    return ptr nullptr
+})",
+                   R"(
+func i1 @main() {
+  %entry:
+    %call.result = call ptr @makePtr
+    return i1 0
+}
+func ptr @makePtr() {
+  %entry:
+    return ptr nullptr
+})");
+}
+
+TEST_CASE("Compare unknown pointer to non-escaping dynamic allocation",
+          "[opt][pointer-analysis]") {
+    test::passTest("ptranalysis, instcombine, simplifycfg",
+                   R"(
+ext func { ptr, i64 } @__builtin_alloc(i64 %0, i64 %1)
+ext func void @__builtin_dealloc(ptr %0, i64 %1, i64 %2)
+func i1 @main() {
+  %entry:
+    %unique.alloc = call { ptr, i64 } @__builtin_alloc, i64 8, i64 8
+    %unique.pointer = extract_value { ptr, i64 } %unique.alloc, 0
+    %call.result = call ptr @makePtr
+    %eq = ucmp eq ptr %unique.pointer, ptr %call.result
+    %unique.ptr.engaged = ucmp neq ptr %unique.pointer, ptr nullptr
+    branch i1 %unique.ptr.engaged, label %unique.ptr.delete, label %unique.ptr.end
+
+  %unique.ptr.delete: // preds: entry
+    call void @__builtin_dealloc, ptr %unique.pointer, i64 8, i64 8
+    goto label %unique.ptr.end
+
+  %unique.ptr.end: // preds: entry, unique.ptr.delete
+    return i1 %eq
+}
+func ptr @makePtr() {
+  %entry:
+    return ptr nullptr
+})",
+                   R"(
+ext func { ptr, i64 } @__builtin_alloc(i64 %0, i64 %1)
+ext func void @__builtin_dealloc(ptr %0, i64 %1, i64 %2)
+func i1 @main() {
+  %entry:
+    %unique.alloc = call { ptr, i64 } @__builtin_alloc, i64 8, i64 8
+    %unique.pointer = extract_value { ptr, i64 } %unique.alloc, 0
+    %call.result = call ptr @makePtr
+    call void @__builtin_dealloc, ptr %unique.pointer, i64 8, i64 8
+    return i1 0
+}
+func ptr @makePtr() {
+  %entry:
+    return ptr nullptr
+})");
 }
